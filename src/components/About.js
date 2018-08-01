5ae69d568e9d15a19c94f2a0416adb50481fcb84
Mario Torres <mario291187@gmail.com>
2018-08-01 11:58:00 -0500

    About

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 5
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        //textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
 
 
 class About extends React.Component {
 
     render(){
+        const { classes } = this.props;
+
         return (
-        <Typography>
-            Acerca de ...
-        </Typography>
+            <div className={classes.root}>
+                <Grid container spacing={24}>
+                    <Grid item xs={12}>
+                        <Paper className = {classes.paper}>
+                            <Typography>
+                                Acerca de ...
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
         );
     }
 }
 
-export default About;
+About.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(About);
