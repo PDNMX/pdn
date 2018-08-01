a54765d181331d8440a04953de93c556b54bcf44
Mario Torres <mario291187@gmail.com>
2018-08-01 11:29:22 -0500

    FAQ

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 5
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        //textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+});
 
 class Faq extends React.Component{
     render (){
+        const { classes } = this.props;
         return (
-            <Typography>Preguntas frecuentes</Typography>
+            <div className={classes.root}>
+                <Grid container spacing={24}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <Typography>Preguntas frecuentes</Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
         );
     }
 }
 
+Faq.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
-export default Faq;
+export default withStyles(styles)(Faq);
