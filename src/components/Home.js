f1e36d52d28f19ae6075fdc4cbb3ca6c8d3f335b
Mario Torres <mario291187@gmail.com>
2018-08-01 10:38:41 -0500

    PDN

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import '../App.css';
+import './home.css';
 
 class Home extends React.Component{
     render (){
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">PDN</h1>
+                    <h1 className="App-title">Plataforma Digital Nacional</h1>
+                    <p> Inteligencia de datos anticorrupción</p>
                 </header>
                 <p className="App-intro">
-                    Landing page
+                    Explora los sistemas de la PDN
                 </p>
             </div>
         );
