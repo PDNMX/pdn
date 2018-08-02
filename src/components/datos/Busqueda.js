5440dd07edc9ce26de876731370af0de7561f4bd
Mario Torres <mario291187@gmail.com>
2018-08-02 10:59:39 -0500

    Datos

---
+++ src/components/datos/Busqueda.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import {withStyles} from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        //marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 500,
+    },
+    menu: {
+        width: 200,
+    },
+});
+
+class Busqueda extends React.Component{
+
+    /*
+    state = {
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+    };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };*/
+
+
+    render (){
+        const { classes } = this.props;
+
+        return (
+            <form className={classes.container} noValidate autocomplete='off'>
+                <TextField
+                    id="search"
+                    label="Buscar datos"
+                    type="search"
+                    className={classes.textField}
+                    margin="normal"
+                />
+            </form>
+        );
+    }
+}
+
+Busqueda.propTypes = {
+    classes : PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Busqueda);
--- src/components/datos/Datos.js
+++ src/components/datos/Datos.js
@@ -5,11 +5,15 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Tabla from './Tabla';
+import Busqueda from './Busqueda';
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
-        padding: theme.spacing.unit * 5
+        paddingTop: theme.spacing.unit * 5,
+        paddingBottom: theme.spacing.unit * 5,
+        paddingRight: theme.spacing.unit * 25,
+        paddingLeft: theme.spacing.unit * 25,
     },
     paper: {
         padding: theme.spacing.unit * 2,
@@ -36,11 +40,8 @@ class Datos extends React.Component {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </Typography>
 
-
+                        <Busqueda/>
                         <Tabla/>
-
-
-
 
 
                     </Grid>
