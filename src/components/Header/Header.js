992a69aa8cc1a4fb2deac5421a366b145440cdfc
Mario Torres <mario291187@gmail.com>
2018-08-07 12:56:40 -0500

    Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
     render(){
         return (
             <AppBar color="white"  position="static" >
-                <div style={{paddingLeft:'4em',paddingRight:'4em'}}>
+                <div style={{paddingLeft:'100px',paddingRight:'100px'}}>
                     <div className="left">
                         <img src={imgHeader} alt="logoPDN" style={{width:'4em'}}/>
                     </div>
