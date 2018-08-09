d6dbc3407d247b74627501ace8fcc90233c7b371
Mario Torres <mario291187@gmail.com>
2018-08-08 19:05:02 -0500

    P404

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -37,7 +37,12 @@ class Faq extends React.Component{
                     <Grid container spacing={24}>
                         <Grid item xs={12}>
                             <Paper className={classes.paper}>
-                                <Typography>Preguntas frecuentes</Typography>
+                                <Typography variant="title">
+                                    Lorem Ipsum
+                                </Typography>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </Typography>
                             </Paper>
                         </Grid>
                     </Grid>
