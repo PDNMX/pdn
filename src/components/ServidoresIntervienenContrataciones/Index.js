29ffdd71af2ff84e2cf3ec39b97afc86811c9af5
Mario Torres <mario291187@gmail.com>
2018-08-10 10:35:03 -0500

    Servidores que internenen en procesos de Contratación

--- src/components/ServidoresIntervienenContrataciones/Index.js
+++ src/components/ServidoresIntervienenContrataciones/Index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 
 
 const styles = theme => ({
     root: {
-
+        flexGrow: 1
     }
 
 });
 
-
 class Index extends React.Component{
-
-
     render(){
+
+        const {classes} = this.props;
+
         return (
             <Typography>
                 Servidores que intervienen en procesos de contratación
@@ -23,5 +24,8 @@ class Index extends React.Component{
 
 }
 
+Index.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(Index);
