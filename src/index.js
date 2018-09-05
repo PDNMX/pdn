4b8d2c22af4a2b3125ab7dc46799427aa51928ae
Mario Torres <mario291187@gmail.com>
2018-09-05 10:23:18 -0500

    redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-/*
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-   <Router history={hist}>
-
-        <Switch>
-            {pndRoutes.map((prop, key) => {
-                return <Route path={prop.path} component={prop.component} key={key} />;
-            })}
-        </Switch>
-    </Router>,
-    document.getElementById("root")
-
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
-*/
-ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+//import reducers ...
+
+export default combineReducers({
+
+});
