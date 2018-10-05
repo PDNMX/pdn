7c5058bdd1d263de5628e4f2c408f4741f4b018c
Mario Torres <mario291187@gmail.com>
2018-10-04 19:10:32 -0500

    Styling

--- src/components/ServidoresIntervienen/Index.js
+++ src/components/ServidoresIntervienen/Index.js
@@ -19,30 +19,13 @@ import ViewListIcon from "@material-ui/icons/ViewList";
 const styles = theme => ({
     root: {
         flexGrow: 1,
+        backgroundColor: '#e5e5e5',
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh'
     },
     container1: {
         flexGrow: 1,
-        [theme.breakpoints.up('sm')]: {
-            paddingLeft: '100px',
-            paddingRight: '100px',
-            paddingTop: theme.spacing.unit*3,
-            paddingBottom: theme.spacing.unit * 3
-        },
-        [theme.breakpoints.down('sm')]: {
-            paddingLeft: theme.spacing.unit,
-            paddingRight: theme.spacing.unit,
-            paddingTop: theme.spacing.unit,
-            paddingBottom: theme.spacing.unit,
-        },
-        [theme.breakpoints.up('xl')]: {
-            paddingLeft: '300px',
-            paddingRight: '300px',
-            paddingTop: theme.spacing.unit*3,
-            paddingBottom: theme.spacing.unit*3,
-        },
         backgroundColor: theme.palette.backDark.color
     },
     title: {
@@ -63,24 +46,6 @@ const styles = theme => ({
     },
     container2: {
         flexGrow: 1,
-        [theme.breakpoints.up('sm')]: {
-            paddingLeft: '100px',
-            paddingRight: '100px',
-            paddingTop: theme.spacing.unit*3,
-            paddingBottom: theme.spacing.unit * 3
-        },
-        [theme.breakpoints.down('sm')]: {
-            paddingLeft: theme.spacing.unit,
-            paddingRight: theme.spacing.unit,
-            paddingTop: theme.spacing.unit,
-            paddingBottom: theme.spacing.unit,
-        },
-        [theme.breakpoints.up('xl')]: {
-            paddingLeft: '300px',
-            paddingRight: '300px',
-            paddingTop: theme.spacing.unit*3,
-            paddingBottom: theme.spacing.unit*3,
-        },
         backgroundColor: theme.palette.backLight.color,
 
     },
@@ -104,80 +69,84 @@ class Index extends React.Component {
                 <Header/>
 
 
-                <Grid container spacing={0} justify='center'>
-                    <Grid item xs={12} className={classes.section}>
-                        <Typography variant="title" className={classes.title}>
-                            Servidores que intervienen en procesos de contratación
-                        </Typography>
-                        <br/>
-                        <br/>
+                <div className={classes.container2}>
+                    <Grid container spacing={0} justify='center'>
+                        <Grid item xs={12} className={classes.section}>
+                            <Typography variant="title" className={classes.title}>
+                                Servidores que intervienen en procesos de contratación
+                            </Typography>
+                            <br/>
+                            <br/>
 
-                        <Typography className={classes.textDark}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                            irure
-                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
-                        </Typography>
+                            <Typography className={classes.textDark}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure
+                                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </Typography>
+                        </Grid>
+
+
+                        <Grid item xs={12} className={classes.section}>
+                            <ExpansionPanel classes={{root: classes.bgPanelLight}} defaultExpanded={true}>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>} classes={{content: classes.summary}}>
+                                    <ViewListIcon/>
+                                    <Typography>
+                                        Ver tabla
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <TablaServidores/>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+
+                        </Grid>
+
                     </Grid>
+                </div>
 
 
-                    <Grid item xs={12} className={classes.section}>
-                        <ExpansionPanel classes={{root: classes.bgPanelLight}} defaultExpanded={true}>
-                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>} classes={{content: classes.summary}}>
-                                <ViewListIcon/>
-                                <Typography>
-                                    Ver tabla
-                                </Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <TablaServidores/>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
 
+                <div className={classes.container1}>
+                    <Grid container spacing={0} justify='center'>
+                        <Grid item xs={12} className={classes.section}>
+
+                            <Typography variant={'title'} className={classes.title}>Particulares inhabilitados</Typography>
+                            <br/>
+                            <br/>
+
+                            <Typography className={classes.textLight}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure
+                                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </Typography>
+                        </Grid>
+
+                        <Grid item  xs={12} className={classes.section}>
+                            <ExpansionPanel classes={{root: classes.bgPanelDark}} defaultExpanded={true}>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}
+                                                       classes={{content: classes.summary}}>
+                                    <ViewListIcon/>
+                                    <Typography  className={classes.textLight}>
+                                        Ver Tabla
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <TablaParticulares/>
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        </Grid>
                     </Grid>
-
-                </Grid>
-
-
-
-                <Grid container spacing={0} justify='center'>
-                    <Grid item xs={12} className={classes.section}>
-
-                        <Typography variant={'title'} className={classes.title}>Particulares inhabilitados</Typography>
-                        <br/>
-                        <br/>
-
-                        <Typography className={classes.textLight}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                            irure
-                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
-                        </Typography>
-                    </Grid>
-
-                    <Grid item  xs={12} className={classes.section}>
-                        <ExpansionPanel classes={{root: classes.bgPanelDark}} defaultExpanded={true}>
-                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}
-                                                   classes={{content: classes.summary}}>
-                                <ViewListIcon/>
-                                <Typography  className={classes.textLight}>
-                                    Ver Tabla
-                                </Typography>
-                            </ExpansionPanelSummary>
-                            <ExpansionPanelDetails>
-                                <TablaParticulares/>
-                            </ExpansionPanelDetails>
-                        </ExpansionPanel>
-                    </Grid>
-                </Grid>
+                </div>
 
 
                 <div style={{ backgroundColor: '#fff'}}>
