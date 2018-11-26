8da7a8bd5ae7fa1f043684f583f6d5b4f1fa245d
Mario Torres <mario291187@gmail.com>
2018-11-26 16:52:54 -0600

    Links

--- src/components/Home/Sistemas.js
+++ src/components/Home/Sistemas.js
@@ -7,6 +7,7 @@ import s3 from '../../assets/iconos_azul/3_icono.svg';
 import s4 from '../../assets/iconos_azul/4_icono.svg';
 import s5 from '../../assets/iconos_azul/5_icono.svg';
 import s6 from '../../assets/iconos_azul/6_icono.svg';
+import {Link} from 'react-router-dom';
 
 const styles = theme => ({
     root: {
@@ -18,6 +19,10 @@ const styles = theme => ({
     },
     icons: {
         maxWidth: 200
+    },
+    link: {
+        textDecoration: "none",
+
     }
 });
 
@@ -28,34 +33,46 @@ class Sistemas extends React.Component{
             <div className={classes.root}>
                 <Grid container spacing={24} justify='center' className={classes.container}>
                     <Grid item xs={4} align="center">
-                        <img src={s1} alt="Sistema 1" className={classes.icons}/>
-                        <br/>
-                        Declaraciones
+                        <Link to="/home" className={ classes.link }>
+                            <img src={s1} alt="Sistema 1" className={classes.icons}/>
+                            <br/>
+                            Declaraciones
+                        </Link>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <img src={s2} alt="Sistema 2" className={classes.icons}/>
-                        <br/>
-                        Servidores públicos en contrataciones públicas
+                        <Link to="/servidores" className={ classes.link }>
+                            <img src={s2} alt="Sistema 2" className={classes.icons}/>
+                            <br/>
+                            Servidores públicos en contrataciones públicas
+                        </Link>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <img src={s3} alt="Sistema 3" className={classes.icons}/>
-                        <br/>
-                        Sancionados
+                        <Link to="/sancionados" className ={ classes.link }>
+                            <img src={s3} alt="Sistema 3" className={classes.icons}/>
+                            <br/>
+                            Sancionados
+                        </Link>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <img src={s4} alt="Sistema 4" className={classes.icons}/>
-                        <br/>
-                        Fiscalización
+                        <Link to="/home" className={classes.link }>
+                            <img src={s4} alt="Sistema 4" className={classes.icons}/>
+                            <br/>
+                            Fiscalización
+                        </Link>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <img src={s5} alt="Sistema 5" className={classes.icons}/>
-                        <br/>
-                        Denuncias
+                        <Link to="/home" className={classes.link }>
+                            <img src={s5} alt="Sistema 5" className={classes.icons}/>
+                            <br/>
+                            Denuncias
+                        </Link>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <img src={s6} alt="Sistema 6" className={classes.icons}/>
-                        <br/>
-                        Contrataciones
+                        <Link to="/home" className={classes.link}>
+                            <img src={s6} alt="Sistema 6" className={classes.icons}/>
+                            <br/>
+                            Contrataciones
+                        </Link>
                     </Grid>
                 </Grid>
             </div>
