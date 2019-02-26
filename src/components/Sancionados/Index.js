1b7ad4e0ad0f67d3146deb93e88f1694bb97aa9a
Mario Torres <mario291187@gmail.com>
2019-02-25 18:44:17 -0600

    Servidores

--- src/components/Sancionados/Index.js
+++ src/components/Sancionados/Index.js
@@ -17,14 +17,8 @@ const styles = theme => ({
     root: {
         flexGrow: 1
     },
-    title: {
-        color: theme.palette.black.color,
-        maxHeight: '20px',
-        paddingBottom: '56px'
-    },
     whiteText: {
-        color: theme.palette.titleBanner.color,
-        minHeight: '100px'
+        color: '#fff'
     },
     textDark: {
         color: theme.palette.textNormal,
@@ -38,13 +32,10 @@ const styles = theme => ({
         maxWidth: '1200px'
     },
     image: {
-        paddingBottom: '24px',
-        paddingTop: '28px',
-        minWidth: '230px'
+        width: '60px'
     },
     bgContainer: {
-        backgroundColor: theme.palette.azul.color,
-        paddingTop: '102px',
+        backgroundColor: '#34b3eb',//theme.palette.azul.color,
     },
     center: {
         textAlign: 'center'
@@ -53,24 +44,27 @@ const styles = theme => ({
         backgroundColor: theme.palette.grisTenue.color
     },
     card: {
-        backgroundColor: theme.palette.azul.color,
-        borderRadius: '5px 5px 0px 0px',
-        paddingBottom: '90px',
-        paddingRight: '40px',
-        paddingLeft: '40px',
-        maxWidth: '230px',
+        backgroundColor: '#34b3eb', //theme.palette.azul.color,
+        paddingLeft: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
         margin: 0,
-        display: 'inline-block'
     },
     cardSeleccionada: {
-        backgroundColor: theme.palette.white.color,
-        borderRadius: '5px 5px 0px 0px',
-        paddingBottom: '90px',
-        paddingRight: '40px',
-        paddingLeft: '40px',
-        maxWidth: '230px',
+        backgroundColor: 'grey',//theme.palette.white.color,
+        paddingLeft: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
         margin: 0,
-        display: 'inline-block'
+    },
+    figure: {
+        display: 'inline-block',
+        float: 'left',
+        margin: 0,
+        padding: 0,
+        paddingRight: '8px'
     }
 });
 
@@ -88,83 +82,55 @@ class Index extends React.Component {
             <div className={classes.root}>
                 <Header/>
 
-                {/*
-                <div className={classes.banner}>
-                    <img alt="PDN" className={classes.bgImg} src={imgBanner}/>
-                    <Grid container justify={"center"} spacing={0}>
-                        <Grid item xs={12} className={classes.section} style={{paddingTop: 150}}>
-                            <Grid container spacing={24}>
-                                <Grid item xs={12} sm={6}>
-                                    <Typography variant="h2" className={classes.titleLight}>
-                                        Servidores públicos <br/>y particulares sancionados
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <Typography variant="h6" className={classes.titleSub}>
-                                        Aquí puedes consultar, visualizar y descargar datos de:<br/>
-                                        Las sanciones o inhabilitación firmes en contra de servidores públicos por actos
-                                        vinculados con faltas administrativas graves,<br/>
-                                        Los particulares que están inhabilitados o cuentan con impedimentos para ser
-                                        contratados como servidores públicos, y<br/>
-                                        Las persona físicas que están inhabilitadas o cuentan con impedimentos para ser
-                                        contratistas o prestadores de servicios en el gobierno por faltas
-                                        administrativas graves.
-                                    </Typography><br/>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </div>
-                */}
+                <Grid container spacing={0} justify="center" className={classes.bgContainer}>
+                    <Grid item xs={12} className={classes.section}>
+                        <Grid container spacing={0}>
 
-                <div id="menu" className={classes.bgContainer}>
-                    <Grid container spacing={0} justify="center">
-                        <Grid item xs={12} className={classes.section}>
-                            <Grid container spacing={0}>
-                                <Grid item md={4} xs={12} className={classes.center}>
-                                    <div
-                                        className={this.state.idContent !== 1 ? classes.card : classes.cardSeleccionada}>
-                                        <img src={img1} alt="Servidores públicos sancionados"
-                                             className={classes.image}
-                                             onClick={() => this.changeContent(1)}/>
-                                        <Typography variant={this.state.idContent === 1 ? "h5" : "h6"}
-                                                    className={classes.whiteText}>
-                                            Servidores públicos sancionados
-                                        </Typography>
-                                    </div>
-                                </Grid>
-                                <Grid item md={4} xs={12} className={classes.center}>
-                                    <div
-                                        className={this.state.idContent !== 2 ? classes.card : classes.cardSeleccionada}>
-                                        <img src={img2} alt="Particulares sancionados"
-                                             className={classes.image}
-                                             onClick={() => this.changeContent(2)}/>
-                                        <Typography variant={this.state.idContent === 2 ? "h5" : "h6"}
-                                                    className={classes.whiteText}>
-                                            Particulares sancionados
-                                        </Typography>
-                                    </div>
-
-
-                                </Grid>
-                                <Grid item md={4} xs={12} className={classes.center}>
-                                    <div
-                                        className={this.state.idContent !== 3 ? classes.card : classes.cardSeleccionada}>
-                                        <img src={img3} alt="Visualizaciones"
-                                             className={classes.image}
-                                             onClick={() => this.changeContent(3)}/>
-                                        <Typography variant={this.state.idContent === 3 ? "h5" : "h6"}
-                                                    className={classes.whiteText}>
-                                            Visualizaciones
-                                        </Typography>
-                                    </div>
-                                </Grid>
+                            <Grid item md={4} xs={12} className={this.state.idContent !== 1 ? classes.card : classes.cardSeleccionada} onClick={() => this.changeContent(1)}>
+                                <figure className={classes.figure}>
+                                    <img src={img1} alt="Servidores públicos sancionados"
+                                         className={classes.image}/>
+                                </figure>
+                                <Typography variant= "subtitle1"
+                                            style={{fontWeight: this.state.idContent === 1? 500: 300}}
+                                            className={classes.whiteText}>
+                                    Servidores públicos sancionados
+                                </Typography>
                             </Grid>
 
-                        </Grid>
-                    </Grid>
+                            <Grid item md={4} xs={12} className={this.state.idContent !== 2 ? classes.card : classes.cardSeleccionada} onClick={() => this.changeContent(2)}>
 
-                </div>
+                                <figure className={classes.figure}>
+                                <img src={img2} alt="Particulares sancionados"
+                                     className={classes.image}/>
+                                </figure>
+
+                                <Typography variant= "subtitle1"
+                                            style={{fontWeight: this.state.idContent === 2? 500: 300}}
+                                            className={classes.whiteText}>
+                                    Particulares sancionados
+                                </Typography>
+
+                            </Grid>
+                            <Grid item md={4} xs={12} className={this.state.idContent !== 3 ? classes.card : classes.cardSeleccionada}  onClick={() => this.changeContent(3)}>
+
+                                <figure className={classes.figure}>
+                                <img src={img3} alt="Visualizaciones"
+                                     className={classes.image}/>
+                                </figure>
+
+                                <Typography variant= "subtitle1"
+                                            style={{fontWeight: this.state.idContent === 3? 500: 300}}
+                                            className={classes.whiteText}>
+                                    Visualizaciones
+                                </Typography>
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+
+
 
 
                 <div className={classes.bgPanelTable}>
