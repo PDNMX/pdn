cf9c497d3ca9dcad9f60260d19f01090fc33d85f
IselaMorquecho <isela.escom@gmail.com>
2019-05-07 10:38:48 -0500

    sancionados/dashboard esqueleto

--- src/components/Sancionados/dashboard/Dashboard.js
+++ src/components/Sancionados/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {withStyles} from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
+import Header from "./Header";
+import Grid from "@material-ui/core/Grid/Grid";
+import Footer from "../../Home/Footer";
 
 const styles = theme => ({
     root: {
@@ -8,18 +11,22 @@ const styles = theme => ({
     }
 });
 
-class Dashboard extends React.Component{
+class Dashboard extends React.Component {
     render(){
         const {classes} = this.props;
-
         return (
             <div className={classes.root}>
-
+                <Header/>
+                <Grid container spacing={0} justify='center'>
+                    <Grid item xs={12} className={classes.section}>
+                    </Grid>
+                </Grid>
+                <Footer/>
             </div>
         )
     }
-}
 
+}
 
 Dashboard.propTypes = {
     classes: PropTypes.object.isRequired
