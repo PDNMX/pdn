9a2791e323956182922c82340daac01d99e12bf4
Mario Torres <mario291187@gmail.com>
2019-05-28 18:15:13 -0500

    Metodología de Calidad

--- src/components/GAA/Calidad/Metodologia.js
+++ src/components/GAA/Calidad/Metodologia.js
@@ -4,6 +4,8 @@ import Header from "./Header/Header";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
+import Pipeline from '../../../assets/DQ_pipeline_analy.png';
+import Pasos from '../../../assets/pasos_evaluación.png';
 
 const styles = theme => ({
     root :{
@@ -39,6 +41,12 @@ const styles = theme => ({
     sublist: {
         color: '#606060',
         fontSize: '0.975rem',
+    },
+    pipeline:{
+        maxWidth: '95%'
+    },
+    pasos:{
+        maxWidth: '90%'
     }
 });
 
@@ -125,8 +133,122 @@ class Metodologia extends React.Component{
                         <b>Pasos para la evaluación de calidad de los datos Guía de Apertura Anticorrupción</b>
                     </Typography>
 
+                    <img src={Pasos} alt="Pasos" className={classes.pasos}/>
 
 
+                    <Typography variant="h5">Propuesta de métricas de calidad</Typography>
+                    <Typography paragraph>
+                        Las métricas de calidad de los datos toman en consideración:
+                    </Typography>
+                    <ul>
+                        <li><Typography>La disponibilidad de los datos;</Typography></li>
+                        <li><Typography>Sus procesos de recolección y actualización, y</Typography></li>
+                        <li><Typography>Sus formatos</Typography></li>
+
+                    </ul>
+
+                    <Typography paragraph>
+                        El proceso para la evaluación de los datos que proponemos está alineado a la forma natural del ciclo de vida de los datos: desde su origen hasta su aprovechamiento analítico. Este  proceso está compuesto por los siguientes rubros:
+                    </Typography>
+
+                    <ul className={classes.ul}>
+                        <li><Typography>1. Disponibilidad;</Typography></li>
+                        <li><Typography>2. Integridad;</Typography></li>
+                        <li><Typography>3. Validez y confiabilidad</Typography></li>
+                        <li><Typography>4. Procesabilidad, y</Typography></li>
+                        <li><Typography>5. Relevancia Analítica.</Typography></li>
+                    </ul>
+
+                    <img src={Pipeline} alt="Evaluación" className={classes.pipeline}/>
+
+                    <Typography paragraph>
+                        A continuación se enlistan las características que serán evaluadas en cada rubro. Cada concepto se calificará con 1 si es satisfactorio o 0 en el caso contrario (en algunos es posible tener el valor de 0.5). Las características marcadas en azul son relevantes pero se necesita el apoyo de la entidad para poder evaluarlas.
+                    </Typography>
+
+                    <Typography paragraph>
+                        Que el conjunto de datos:
+                    </Typography>
+
+
+                    <Typography>
+                        <b>1. Disponibilidad:</b>
+                    </Typography>
+
+                    <ul>
+                        <li><Typography>Pueda descargarse desde un sitio público del gobierno;</Typography></li>
+                        <li><Typography>Pueda encontrarse fácilmente;</Typography></li>
+                        <li><Typography>Cuenten con un respaldo en un sitio independiente;</Typography></li>
+                    </ul>
+
+                    <Typography>
+                        <b>2. Integridad:</b>
+                    </Typography>
+                    <ul>
+                        <li><Typography>Se encuentre guardado en un sitio seguro;</Typography></li>
+                        <li><Typography>Que existan mecanismos de seguridad para controlar los cambios de a los datos;</Typography></li>
+                    </ul>
+
+                    <Typography>
+                        <b>3. Validez y confiabilidad:</b>
+                    </Typography>
+                    <ul>
+                        <li><Typography>Provenga de fuentes confiables;</Typography></li>
+                        <li><Typography>Que no existan registros en blanco o errores en los registros;</Typography></li>
+                        <li><Typography>Que las personas recolectoras están calificadas y sean supervisadas;</Typography></li>
+                        <li><Typography>Que existan procedimientos para la recolección, revisión, publicación y actualización periódica de los datos y que estos sean documentados;</Typography></li>
+                    </ul>
+
+                    <Typography>
+                        <b>4. Procesabilidad:</b>
+                    </Typography>
+                    <ul>
+                        <li><Typography>Esté en formato de datos legible por computadora (.csv, .json, etc);</Typography></li>
+                        <li><Typography>Se encuentre en formato no propietario;</Typography></li>
+                        <li><Typography>Pueda bajarse en bulk;</Typography></li>
+                    </ul>
+
+                    <Typography>
+                        <b>5. Relevancia analítica:</b>
+                    </Typography>
+                    <ul>
+                        <li><Typography>Que haya un número considerable de observaciones;</Typography></li>
+                        <li><Typography>Que el conjunto de datos contengan variables suficientes para medir lo que se supone deben medir;</Typography></li>
+                        <li><Typography>Que el conjunto de datos esté actualizados;</Typography></li>
+                    </ul>
+                    <Typography>
+                        <b>5.1 Consistencia:</b>
+                    </Typography>
+                    <ul>
+                        <li><Typography>Que las variables estén codificadas adecuadamente;</Typography></li>
+                    </ul>
+                    <Typography><b>5.2 Completitud:</b></Typography>
+                    <ul>
+                        <li><Typography>Que los datos faltantes estén marcados, y</Typography></li>
+                        <li><Typography>Calidad del diccionario de datos (0 no tiene, 5 sí tiene pero es mejorable, 10 diccionario de buena calidad)</Typography></li>
+                    </ul>
+
+
+                    <Typography variant="h5"> Evaluación </Typography>
+                    <Typography paragraph>
+                        La calidad de cada conjunto de datos fue evaluada usando los rubros anteriormente mencionados. La evaluación detallada de los conjuntos de datos de la Guía puede encontrarse aquí.
+                    </Typography>
+
+                    <Typography variant="h5"> Hallazgos y recomendaciones</Typography>
+
+                    <Typography paragraph>
+                        Estaremos publicando próximamente el análisis realizado y los siguientes pasos. Actualmente nos encontramos notificando a las instituciones el estatus de sus datos.
+                    </Typography>
+
+                    <Typography variant="h5"> Fuentes:</Typography>
+
+                    <ul className={classes.ul}>
+                        <li><Typography>1. Maydanchik, Arkady. Data quality assessment. Technics publications, 2007.</Typography></li>
+                        <li><Typography>2. https://www.techopedia.com/definition/30610/data-quality-assessment-dqauot</Typography></li>
+                        <li><Typography>3. http://web.mit.edu/tdqm/www/tdqmpub/PipinoLeeWangCACMApr02.pdf</Typography></li>
+                        <li><Typography>4. https://ckan.org/2011/01/20/data-quality-what-is-it/</Typography></li>
+                        <li><Typography>5. https://www.usaid.gov/sites/default/files/documents/1861/DQA_Training_and_Preparation_11-17-14.ppt</Typography></li>
+                        <li><Typography>6. https://www.usaid.gov/sites/default/files/documents/1868/597sad.pdf</Typography></li>
+                    </ul>
                 </Grid>
             </Grid>
         </div>
