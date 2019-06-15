5c351c4553a87ca6a6b82935c55be9b54c6a6dc4
Mario Torres <mario291187@gmail.com>
2019-06-15 11:25:46 -0500

    Barra

--- src/components/Home/BarraFea.js
+++ src/components/Home/BarraFea.js
@@ -1,6 +1,5 @@
 import React from 'react';
-//import Typography from '@material-ui/core/Typography';
-//import Grid from '@material-ui/core/grid';
+import Grid from '@material-ui/core/grid';
 import {withStyles} from "@material-ui/core/styles";
 import Blog from '../../assets/iconos_barra/Blog.svg';
 import Calidad from '../../assets/iconos_barra/Calidad_de_datos.svg';
@@ -8,23 +7,36 @@ import Espe from '../../assets/iconos_barra/Especificaciones.svg';
 import Mesa from '../../assets/iconos_barra/Mesa_de_ayuda.svg';
 import Plat from '../../assets/iconos_barra/Plataformas_y_sistemas.svg';
 import {Link} from 'react-router-dom';
+import Typography from "@material-ui/core/Typography";
 
 
 const styles = theme => ({
     root: {
         flexGrow:1,
         backgroundColor: '#f6f6f6',
-        textAlign: 'center',
+        textAlign: 'center'
     },
     icon :{
         maxWidth: 60,
-        paddingTop: 30,
-        paddingBottom: 30,
+        paddingTop: 20,
+        paddingBottom: 0,
         paddingRight: 50,
         paddingLeft: 50,
+
+    },
+    innerContainer: {
+        maxWidth: 1200
+    },
+    link: {
+        textDecoration: "none",
+        color: "#666666",
+
+    },
+    item :{
         '&:hover': {
             backgroundColor: '#c5c5c5'
-        }
+        },
+        paddingBottom: 20
     }
 
 });
@@ -34,19 +46,47 @@ class BarraFea extends React.Component{
     render(){
         const {classes} = this.props;
         return (<div className={classes.root}>
-            <Link to="/mesa-de-ayuda">
-                <img src={Mesa} className={classes.icon} alt="Mesa de ayuda"/>
-            </Link>
-            <a href="https://www.plataformadigitalnacional.org/blog">
-                <img src={Blog} className={classes.icon} alt="Blog"/>
-            </a>
-            <Link to="/gaa/calidad">
-                <img src={Calidad} className={classes.icon} alt="Calidad de datos"/>
-            </Link>
-            <Link to="/especificaciones">
-                <img src={Espe} className={classes.icon} alt="Especificaciones técnicas"/>
-            </Link>
-            <img src={Plat} className={classes.icon} alt="Plataformas y sistemas"/>
+
+            <Grid container spacing={0} justify="center" className={classes.container}>
+                <Grid container spacing={0} justify="center" className={classes.innerContainer}>
+                    <Grid item xs={4} md={2} className={classes.item}>
+                        <Link to="/mesa-de-ayuda" className={classes.link}>
+                            <img src={Mesa} className={classes.icon} alt="Mesa de ayuda"/>
+                            <Typography>Mesa de ayuda</Typography>
+                        </Link>
+
+                    </Grid>
+                    <Grid item xs={4} md={2} className={classes.item}>
+                        <a href="https://www.plataformadigitalnacional.org/blog" className={classes.link}>
+                            <img src={Blog} className={classes.icon} alt="Blog"/>
+                            <Typography>Blog</Typography>
+                        </a>
+                    </Grid>
+
+                    <Grid item xs={4} md={2} className={classes.item}>
+                        <Link to="/gaa/calidad" className={classes.link}>
+                            <img src={Calidad} className={classes.icon} alt="Calidad de datos"/>
+                            <Typography>Calidad de datos</Typography>
+                        </Link>
+
+                    </Grid>
+                    <Grid item xs={4} md={2} className={classes.item}>
+                        <Link to="/especificaciones" className={classes.link}>
+                            <img src={Espe} className={classes.icon} alt="Especificaciones técnicas"/>
+                            <Typography>Especificaciones técnicas</Typography>
+                        </Link>
+                    </Grid>
+
+                    <Grid item xs={4} md={2} className={classes.item}>
+                        <a href="#" className={classes.link}>
+                            <img src={Plat} className={classes.icon} alt="Plataformas y sistemas"/>
+                            <Typography>Plataformas y sistemas</Typography>
+                        </a>
+                    </Grid>
+
+                </Grid>
+            </Grid>
+
 
         </div>);
 
