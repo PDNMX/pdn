4c3040a997534ecd0e7ee95dd29e509023ce705e
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-07-04 13:49:42 -0500

    se agrega parametro exact

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,150 +33,179 @@ import GAA from "../components/GAA/GAA";
 
 const pdnRoutes = [
   {
+    exact: true,
     path: "/",
     component: Home,
     private: false
   },
   {
+    exact: true,
     path: "/about",
     component: About,
     private: false
   },
   {
+    exact: true,
     path: "/terminos",
     component: Terminos,
     private: false
   },
   {
+    exact: true,
     path: "/faq",
     component: Faq,
     private: false
   },
   {
+    exact: true,
     path: "/CatalogoDatos",
     component: Datos,
     private: false
   },
   {
+    exact: true,
     path: "/sancionados",
     component: Sancionados,
     private: false
   },
   {
+    exact: true,
     path: "/sancionados/dashboard",
     component: DashboardSancionados,
     private: false
   },
   {
+    exact: true,
     path: "/sancionados/dashboardParticulares",
     component: DashboardParticularesSancionados,
     private: false
   },
   {
+    exact: true,
     path: "/servidores",
     component: Servidores,
     private: false
   },
   {
+    exact: true,
     path: "/indicadores",
     component: Indicadores,
     private: false
   },
   {
+    exact: true,
     path: "/contrataciones",
     component: Contrataciones,
     private: false
   },
   {
+    exact: true,
     path: "/denuncias",
     component: Denuncias
   },
   {
+    exact: true,
     path: "/formDenuncia",
     component: FormularioDenuncias,
     private: false
   },
   {
+    exact: true,
     path: "/formConsultaDenuncia",
     component: FormularioConsula,
     private: false
   },
   {
+    exact: true,
     path: "/conexion",
     component: Conexion,
     private: false
   },
   {
+    exact: true,
     path: "/especificaciones",
     component: Especificaciones,
     private: false
   },
   {
+    exact: true,
     path: "/declaraciones/especificaciones",
     component: DeclaracionesEspecificaciones,
     private: false
   },
   {
+    exact: true,
     path: "/sancionados/especificaciones",
     component: EspecificacionesSancionados,
     private: false
   },
   {
+    exact: true,
     path: "/intervienen/especificaciones",
     component: EspecificacionesServidoresIntervienen,
     private: false
   },
   {
+    exact: true,
     path: "/Validador",
     component: Validador,
     private: false
   },
   {
+    exact: true,
     path: "/administracionPDN/consultasolicitudes",
     component: Consulta,
     private: false,
     perfom: "admon-pdn-consultaSolicitudes:visit"
   },
   {
+    exact: true,
     path: "/consolaAdmonSO",
     component: ConsolaAdministracionSO,
     private: false,
     perfom: "admon-conexion-so:visit"
   },
   {
+    exact: true,
     path: "/administracionPDN",
     component: ConsolaAdministracionPDN,
     private: false,
     perfom: "admon-pdn-page:visit"
   },
   {
+    exact: true,
     path: "/administracionPDN/usuarios",
     component: ConsultaUsuarios,
     private: false,
     perfom: "admon-pdn-usuarios:visit"
   },
   {
+    exact: true,
     path: "/administracionPDN/consultaconexiones",
     component: ConsultaConexiones,
     private: false,
     perfom: "admon-pdn-consultaConexiones:visit"
   },
   {
+    exact: true,
     path: "/gaa",
     component: GAA,
     private: false
   },
   {
+    exact: true,
     path: "/gaa/calidad",
     component: Metodologia,
     private: false
   },
   {
+    exact: true,
     path: "/mesa-de-ayuda",
     component: HelpDesk,
     private: false
   },
   {
+    exact: false,
     path: "/declaraciones",
     component: Declaraciones,
     private: false
