df45645558ce3249b8e079ea3c73c40d29d8fa25
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-07-04 15:40:30 -0500

    declaraciones path

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -60,102 +60,145 @@ class Footer extends React.Component{
     render(){
         const {classes} = this.props;
         return (
-            <div className={classes.root}>
-
-                <Grid container spacing={0} className={classes.parteGris} justify='center'>
-                    <Grid item xs={12} className={classes.item}>
-
-                        <Grid container spacing={0}>
-                            <Grid item xs={12} md={6}>
-                                {/*<a href="https://www.gob.mx/sesna">
+          <div className={classes.root}>
+            <Grid
+              container
+              spacing={0}
+              className={classes.parteGris}
+              justify="center"
+            >
+              <Grid item xs={12} className={classes.item}>
+                <Grid container spacing={0}>
+                  <Grid item xs={12} md={6}>
+                    {/*<a href="https://www.gob.mx/sesna">
                                     <img alt="SESNA" src={LogoSesna} className={classes.logoSESNA}/>
                                 </a>*/}
 
-                                <img alt="PDN" src={LogoPDN} className={classes.logoPDN}/>
+                    <img
+                      alt="PDN"
+                      src={LogoPDN}
+                      className={classes.logoPDN}
+                    />
 
-                                <Typography className={classes.grayText} paragraph>
-                                    <b>Plataforma Digital Nacional</b>
-                                </Typography>
-                                <br/>
-                                <br/>
-                                <Typography className={classes.grayText}  style={{
-                                    fontSize: '12px',
-                                    fontStyle: 'normal',
-                                    fontWeight: '400',
-                                    fontFamily: 'Noto Sans SC'
-                                }} paragraph>Inteligencia de Datos Anticorrupción</Typography>
-                                <br/>
-                                <img alt="Libre Uso MX " src ={LibreUso} className={classes.libreUso}/>
-                                <br/>
-                                <img alt="Open Knowledge" src={OK}/>
-                            </Grid>
-                            <Grid item xs={6} md={3}>
-                                <Typography className={classes.blueText} paragraph>
-                                    <b>Sistemas</b>
-                                </Typography>
-                                <Typography className={classes.grayText} paragraph>
-                                    <a href="https://demospdn.host/declaraciones/" className={classes.grayText}> Declaraciones</a>
-                                </Typography>
-                                <Link to="/servidores" className={classes.link}>
-                                    <Typography className={classes.grayText} paragraph>
-                                        Servidores en contrataciones
-                                    </Typography>
-                                </Link>
-                                <Link to="/sancionados" className={classes.link}>
-                                    <Typography className={classes.grayText} paragraph>
-                                        Sancionados
-                                    </Typography>
-                                </Link>
-                                {/*
+                    <Typography className={classes.grayText} paragraph>
+                      <b>Plataforma Digital Nacional</b>
+                    </Typography>
+                    <br />
+                    <br />
+                    <Typography
+                      className={classes.grayText}
+                      style={{
+                        fontSize: "12px",
+                        fontStyle: "normal",
+                        fontWeight: "400",
+                        fontFamily: "Noto Sans SC"
+                      }}
+                      paragraph
+                    >
+                      Inteligencia de Datos Anticorrupción
+                    </Typography>
+                    <br />
+                    <img
+                      alt="Libre Uso MX "
+                      src={LibreUso}
+                      className={classes.libreUso}
+                    />
+                    <br />
+                    <img alt="Open Knowledge" src={OK} />
+                  </Grid>
+                  <Grid item xs={6} md={3}>
+                    <Typography className={classes.blueText} paragraph>
+                      <b>Sistemas</b>
+                    </Typography>                    
+                    <Link to="/declaraciones" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        Declaraciones
+                      </Typography>
+                    </Link>
+                    <Link to="/servidores" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        Servidores en contrataciones
+                      </Typography>
+                    </Link>
+                    <Link to="/sancionados" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        Sancionados
+                      </Typography>
+                    </Link>
+                    {/*
                                 <Typography className={classes.grayText}>Fiscalización</Typography>
                                 <Typography className={classes.grayText}>Denuncias</Typography>*/}
-                                <Link to="/contrataciones" className={classes.link}>
-                                    <Typography className={classes.grayText}>Contrataciones</Typography>
-                                </Link>
-                            </Grid>
-                            <Grid item xs={6} md={3}>
-                                <Typography className={classes.blueText} paragraph>
-                                    <b>PDN</b>
-                                </Typography>
-                                <Typography paragraph>
-                                    <a href="https://www.plataformadigitalnacional.org/blog" className={classes.grayText}>Blog</a>
-                                </Typography>
+                    <Link to="/contrataciones" className={classes.link}>
+                      <Typography className={classes.grayText}>
+                        Contrataciones
+                      </Typography>
+                    </Link>
+                  </Grid>
+                  <Grid item xs={6} md={3}>
+                    <Typography className={classes.blueText} paragraph>
+                      <b>PDN</b>
+                    </Typography>
+                    <Typography paragraph>
+                      <a
+                        href="https://www.plataformadigitalnacional.org/blog"
+                        className={classes.grayText}
+                      >
+                        Blog
+                      </a>
+                    </Typography>
 
-                                <Link to="/faq" className={classes.link}>
-                                    <Typography className={classes.grayText} paragraph>
+                    <Link to="/faq" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        Preguntas frecuentes
+                      </Typography>
+                    </Link>
 
-                                        Preguntas frecuentes
+                    <Link to="/about" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        ¿Qué es la PDN?
+                      </Typography>
+                    </Link>
 
-                                    </Typography>
-                                </Link>
-
-                                <Link to="/about" className={classes.link}>
-                                    <Typography className={classes.grayText} paragraph>
-                                        ¿Qué es la PDN?
-                                    </Typography>
-                                </Link>
-
-                                <Link to="/terminos" className={classes.link}>
-                                    <Typography className={classes.grayText} paragraph>
-                                        Términos de uso
-                                    </Typography>
-                                </Link>
-                                {/*<Typography className={classes.grayText}>Contacto</Typography>*/}
-                            </Grid>
-                        </Grid>
-
-                    </Grid>
+                    <Link to="/terminos" className={classes.link}>
+                      <Typography
+                        className={classes.grayText}
+                        paragraph
+                      >
+                        Términos de uso
+                      </Typography>
+                    </Link>
+                    {/*<Typography className={classes.grayText}>Contacto</Typography>*/}
+                  </Grid>
                 </Grid>
+              </Grid>
+            </Grid>
 
-
-                <Grid container spacing={0} justify='center'>
-                    <Grid item xs={12} align="center">
-                        <img alt="SESNA" src={LogoSesna} className={classes.logo}/>
-                    </Grid>
-                </Grid>
-
-            </div>
-        )
+            <Grid container spacing={0} justify="center">
+              <Grid item xs={12} align="center">
+                <img
+                  alt="SESNA"
+                  src={LogoSesna}
+                  className={classes.logo}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        );
     }
 }
 
