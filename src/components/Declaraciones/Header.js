35e43272890bff82e06e7e2650b4c9feb69dee04
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-07-05 10:51:00 -0500

    cambios

--- src/components/Declaraciones/Header.js
+++ src/components/Declaraciones/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+import withWidth, { isWidthUp } from "@material-ui/core/withWidth";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+import PDNLogo from "../../assets/PDN.png";
+
+import "./header.css";
+
 //menu
 import {
   Grid,
@@ -13,15 +21,68 @@ import {
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
-let styles = theme => ({
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
   grow: {
     flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  buttons: {
+    color: "#666666"
   },
   item: {
     maxWidth: 1200
   },
-  menuBtn: {
+  href: {
+    textDecoration: "none",
+    color: "inherit"
+  },
+  blog: {
     textTransform: "none"
+  },
+  nested: {
+    paddingLeft: theme.spacing(4)
+  },
+  container1: {
+    //background: 'grey',
+    paddingTop: "75px",
+    paddingBottom: "75px",
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    position: "relative"
+  },
+  link: {
+    textDecoration: "none",
+    color: "inherit"
+  },
+  item1: {
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2)
+  },
+  item2: {
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2)
+  },
+  item3: {
+    // maxWidth: 1200,
+    padding: "30px"
+  },
+  s2: {
+    maxWidth: "150px"
+  },
+  whiteText: {
+    color: "#fff"
+  },
+  pdnLogo: {
+    maxWidth: 110
+    // paddingLeft: "40px",
+    // paddingTop: "40px",
+    // paddingBottom: "40px"
   }
 });
 
@@ -62,108 +123,121 @@ class Header extends React.Component {
   };
 
   render() {
-    const { classes, logo, titulo, copy } = this.props;
+    const { classes, logo, titulo, copy, background } = this.props;
     let { anchorEl } = this.state;
     let open = Boolean(anchorEl);
 
     return (
-      <div className={classes.grow}>
+      <div className={classes.root}>
         <Grid container spacing={0} justify="center">
-          <Grid item xs={12} className={classes.item}>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="Menu"
-                component={Button}
-                href={`${process.env.PUBLIC_URL + "/"}`}
-                style={{ paddingTop: "45px", paddingBottom: "40px" }}
-              >
-                <span className="pdn_l" style={{ width: "110px" }} />
-              </IconButton>
-              <Typography
-                variant="h6"
-                color="inherit"
-                className={classes.grow}
-              />
-              <IconButton
-                aria-owns={open ? "menu-appbar" : undefined}
-                aria-haspopup="true"
-                onClick={this.handleMenu}
-                color="inherit"
-              >
-                <MenuIcon style={{ fill: "#999", fontSize: "36px" }} />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right"
-                }}
-                open={open}
-                onClose={this.handleClose}
-              >
-                {menu.map(opcion => {
-                  return (
-                    <MenuItem
-                      key={opcion.enlace}
-                      onClick={this.handleClose}
-                      component={Button}
-                      href={`${process.env.PUBLIC_URL + opcion.enlace}`}
-                      className={classes.menuBtn}
-                    >
-                      {opcion.nombre}
-                    </MenuItem>
-                  );
-                })}
-              </Menu>
+          <Grid
+            item
+            xs={12}
+            className={classes.item3}
+            style={{ float: "left" }}
+          >
+            <Link to="/" className={classes.link}>
+              <img src={PDNLogo} alt="PDN" className={classes.pdnLogo} />
+            </Link>
+            <Toolbar style={{ float: "right" }}>
+              <div>
+                <IconButton
+                  aria-owns={open ? "menu-appbar" : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <MenuIcon style={{ fill: "#999", fontSize: "36px" }} />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  {menu.map(opcion => {
+                    return (
+                      <MenuItem
+                        key={opcion.enlace}
+                        component={Link}
+                        to={opcion.enlace}
+                      >
+                        {opcion.nombre}
+                      </MenuItem>
+                    );
+                  })}
+                </Menu>
+              </div>
             </Toolbar>
           </Grid>
         </Grid>
-        <div className="breadcrumb">
-          <div className="container">
-            <Grid container spacing={3}>
-              <Grid item sm={12}>
-                <ul>
-                  <li>
-                    <a href={`${process.env.PUBLIC_URL + "/"}`}>
-                      Plataforma Digital Nacional
-                    </a>
-                  </li>
-                  <li v-if="isProfile">
-                    <a href={process.env.PUBLIC_URL + "/declaraciones"}>
-                      Declaraciones
-                    </a>
-                  </li>
-                </ul>
-              </Grid>
-            </Grid>
-          </div>
-        </div>
-        <div style={{ display: "block" }}>
-          <div className="pdn_b_title">
-            <div className="container">
-              <Grid container justify="center" spacing={3}>
-                <Grid item xs={11} sm={2} style={{ zIndex: 1 }}>
-                  <img alt="logo" src={logo} width="150px" />
-                </Grid>
-                <Grid item xs={11} sm={6} style={{ zIndex: 1 }}>
-                  <h1>
-                    <strong>{titulo}</strong>
-                  </h1>
-                  <p>{copy}</p>
-                </Grid>
-              </Grid>
-            </div>
-          </div>
-        </div>        
+        <Grid container spacing={0} className="breadcrumb" justify="center">
+          <Grid item xs={12}>
+            <ul>
+              <li>
+                <Link className={classes.link} to="/">
+                  Plataforma Digital Nacional
+                </Link>
+              </li>
+              <li>{titulo}</li>
+            </ul>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={0}
+          className={classNames(classes.container1, "servidores")}
+          justify="center"
+        >
+          <img
+            src={background}
+            alt="Sistema 1"
+            style={{ zIndex:-1, maxHeight: "100px" }}
+          />
+          <Grid
+            item
+            xs={12}
+            md={4}
+            align={isWidthUp("md", this.props.width) ? "right" : "center"}
+            className={classes.item1}
+          >
+            <img src={logo} alt="Sistema 2" className={classes.s2} />
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={6}
+            className={classes.item2}
+            align={isWidthUp("md", this.props.width) ? "left" : "center"}
+          >
+            <Typography
+              variant="h4"
+              paragraph
+              className={classes.whiteText}
+              style={{ fontWeight: 600 }}
+            >
+              {titulo}
+            </Typography>
+            <Typography
+              className={classes.whiteText}
+              style={{ fontSize: "18px", fontWeight: 500 }}
+            >
+              {copy}
+            </Typography>
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default withStyles(styles)(Header);
+export default withWidth()(withStyles(styles)(Header));
