d2e4fcbd1751fb2b715cb974802f70ac6b96e7ec
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-07-12 13:27:05 -0500

    test exclude declaraciones

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import {withRouter} from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends React.Component {
-    componentDidUpdate(prevProps) {
-        if (this.props.location.pathname !== prevProps.location.pathname) {
-            window.scrollTo(0, 0);
-        }
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      if (!this.props.location.pathname.includes("/declaraciones/")) {
+        window.scrollTo(0, 0);
+      }
     }
+  }
 
-    render() {
-        return this.props.children;
-    }
+  render() {
+    return this.props.children;
+  }
 }
 
 export default withRouter(ScrollToTop);
