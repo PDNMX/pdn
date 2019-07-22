bd99a2a3455868ff7de27b89e2a4b6f6957c48d5
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-07-22 17:07:45 -0500

    se remueve condicional especial para scrolltop

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -3,10 +3,8 @@ import { withRouter } from "react-router-dom";
 
 class ScrollToTop extends React.Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      if (!this.props.location.pathname.includes("/declaraciones/")) {
-        window.scrollTo(0, 0);
-      }
+      if (this.props.location.pathname !== prevProps.location.pathname) {
+        window.scrollTo(0, 0);        
     }
   }
 
