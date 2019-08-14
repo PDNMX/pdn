cd3b7ae97ea3d1338a3e9199c353ddb7731b1a8c
Mario Torres <mario291187@gmail.com>
2019-08-14 13:26:54 -0500

    S6

--- src/components/Contrataciones/Disclaimer.js
+++ src/components/Contrataciones/Disclaimer.js
@@ -5,22 +5,42 @@ import {withStyles} from "@material-ui/styles";
 const styles = theme => ({
     root: {
         flexGrow: 1
-    }
+    },
+    bullet: {
+        backgroundColor: '#89d4f2',
+        height: '10px',
+        width: '10px',
+        borderRadius: '50%',
+        display: 'inline-block',
+        marginLeft: '-20px',
+        marginRight: "10px",
+        marginBottom: '1px'
+    },
+    ul: {
+        listStyle: 'none',
+        //marginLeft: 0,
+        paddingLeft: '20px'
+    },
+    li: {
+        paddingBottom: theme.spacing(2)
+    },
 });
 
 class Disclaimer extends React.Component{
 
     render() {
+        const {classes} = this.props;
+
         return (
             <div>
                 <Typography paragraph>
                     Aquí encontrarás la siguiente información:
                 </Typography>
 
-                <ul>
-                    <li><Typography>Cuánto gasta el gobierno</Typography></li>
-                    <li><Typography>Qué tipos de procedimientos</Typography></li>
-                    <li><Typography>información sobre los proveedores que participan</Typography></li>
+                <ul className={classes.ul}>
+                    <li className={classes.li}> <Typography><span className={classes.bullet}/>Cuánto gasta el gobierno</Typography></li>
+                    <li className={classes.li}><Typography><span className={classes.bullet}/>Qué tipos de procedimientos</Typography></li>
+                    <li className={classes.li}><Typography><span className={classes.bullet}/>información sobre los proveedores que participan</Typography></li>
                 </ul>
             </div>
         );
