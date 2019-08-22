d3346b36f4871872afed3d9bd6a530d44acb0609
IselaMorquecho <isela.escom@gmail.com>
2019-08-22 17:22:50 -0500

    Icon menu sistemas

--- src/components/Compartidos/MenuSistemas.js
+++ src/components/Compartidos/MenuSistemas.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {withStyles} from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import MenuIcon from "@material-ui/icons/Menu";
+import MenuIcon from "@material-ui/icons/MoreVert";
 import IconButton from "@material-ui/core/IconButton";
 import {Link} from "react-router-dom";
 import withWidth from "@material-ui/core/withWidth/withWidth";
