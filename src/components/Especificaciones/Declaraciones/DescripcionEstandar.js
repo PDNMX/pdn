dbde2d9bdea992c0e90471eb37ec88af47631ef8
Mario Torres <mario291187@gmail.com>
2019-10-17 11:33:51 -0500

    Declaraciones

--- src/components/Especificaciones/Declaraciones/DescripcionEstandar.js
+++ src/components/Especificaciones/Declaraciones/DescripcionEstandar.js
@@ -43,43 +43,32 @@ class DescripcionEstandar extends React.Component {
                 <Typography paragraph color='textPrimary'>
                     Esta guía tiene como finalidad la descripción del Estándar para la Interoperabilidad de Datos de Declaraciones,
                     desarrollado por la Secretaría Ejecutiva del Sistema Nacional Anticorrupción (SESNA).
-                    Se presenta el modelo de interoperabilidad que deberán adoptar los diversos sistemas de declaraciones que proveerán
-                    información a la Plataforma Digital Nacional (PDN) y
-                    se proporciona una serie de recomendaciones para la implementación del estándar por parte de las Instituciones.
+                    Se presenta el modelo de interoperabilidad que deberán adoptar los diversos sistemas de declaraciones que proveerán información a
+                    la Plataforma Digital Nacional (PDN) y se proporciona una serie de recomendaciones para la implementación del estándar por parte de las Instituciones.
                 </Typography>
 
                 <Typography variant="h5" paragraph color='textPrimary'>Antecedentes</Typography>
                 <ul className={classes.ul}>
                     <li className={classes.li}>
                         <Typography paragraph display='inline'>
-                            El 13 de septiembre de 2018, durante la Tercera Sesión Ordinaria 2018 del Comité Coordinador
-                            del Sistema Nacional Anticorrupción (SNA), se aprobó el nuevo Formato Nacional para la Declaración
-                            Patrimonial y de Intereses propuesto por el Comité de Participación Ciudadana (CPC),
-                            el nuevo formato tendrá impacto a nivel nacional y es de presentación obligatoria para todas
-                            las personas que desempeñen un empleo, cargo o comisión en los entes públicos, conforme a lo
-                            dispuesto por el artículo 108 de la Constitución Política de los Estados Unidos Mexicanos
-                            y entrará en vigor, a más tardar el 30 de abril de 2019.
+                            El 23 de octubre de 2018 fueron publicadas las Bases para el funcionamiento
+                            de la Plataforma Digital Nacional (PDN) en el Diario Oficial de la Federación (DOF).
+                            En el art. 6 de las Bases, se establece que para el correcto funcionamiento de cada uno de los sistemas,
+                            la SESNA emitirá los protocolos, estándares, reglamentos, especificaciones técnicas y cualquier normativa necesaria para la colaboración,
+                            provisión de datos y acciones, los cuales serán obligatorios para todos los proveedores, concentradores y encargados a nivel federal, estatal y municipal.
                         </Typography></li>
                     <li className={classes.li}>
                         <Typography paragraph display='inline'>
-                            A su vez, el 23 de octubre de 2018 fueron publicadas las Bases para el funcionamiento de la
-                            Plataforma Digital Nacional (PDN) en el DOF. El Art. 6 establece que para el correcto funcionamiento
-                            de cada uno de los sistemas, la SESNA emitirá los protocolos, estándares, reglamentos, especificaciones
-                            técnicas y cualquier normativa necesaria para la colaboración, provisión de datos y acciones para
-                            cumplir con las Bases, los cuales serán obligatorios para todos los proveedores, concentradores
-                            y encargados a nivel federal, estatal y municipal.
+                            El 10 de julio y el 7 de agosto del 2019, sucedieron la 3º Sesión Ordinaria y la 2º Sesión
+                            Extraordinaria del Comité Coordinador del Sistema Nacional Anticorrupción. El resultado de éstas
+                            es la aprobación a las modificaciones de los formatos que los y las servidoras públicas a nivel nacional utilizaremos para declarar nuestros patrimonios e intereses.
                         </Typography></li>
                     <li className={classes.li}>
                         <Typography paragraph display="inline">
-                            El 11 de noviembre de 2018 se publicó en el DOF el Acuerdo por el que el Comité Coordinador
-                            del Sistema Nacional Anticorrupción emite el formato de declaraciones: de situación patrimonial
-                            y de intereses; y expide las normas e instructivo para su llenado y presentación.
-                            Dicho formato será utilizado por los Servidores Públicos de manera obligatoria para presentar
-                            sus declaraciones de situación patrimonial y de intereses cuando se encuentre operable, esto es,
-                            una vez que sea técnicamente posible la interoperabilidad de los sistemas de evolución patrimonial
-                            y de declaración de intereses, a que hace referencia la fracción I del artículo 49 de la
-                            Ley General del Sistema Nacional Anticorrupción, con la Plataforma Digital Nacional del
-                            Sistema Nacional Anticorrupción, lo que no podrá exceder del 30 de abril del año 2019.
+                            La publicación en el DOF del nuevo formato de declaraciones de situación patrimonial y de intereses sucedió
+                            el 23 de septiembre del 2019. Asimismo, en el segundo transitorio del Acuerdo por el Comité Coordinador emite el formato,
+                            se estableció que las autoridades correspondientes deberán, en los sistemas de información para presentar las declaraciones,
+                            implementar el nuevo formato y garantizar la interoperabilidad con la PDN a más tardar el 31 de diciembre del 2019.
                         </Typography></li>
                 </ul>
 
@@ -88,42 +77,20 @@ class DescripcionEstandar extends React.Component {
                 </Typography>
 
                 <Typography paragraph color="textPrimary">
-                    En la actualidad, la información de declaraciones de los servidores públicos de los diferentes
-                    niveles de gobierno se encuentra contenida en diversos formatos,
-                    bases de datos y sistemas de información; cada uno de ellos con particularidades tecnológicas y
-                    reglas de negocio distintas, dificultando la integración y la
-                    interoperabilidad de los datos. El Estándar para la Interoperabilidad de Datos de Declaraciones surge
-                    a partir de la necesidad de distribuir, comparar y analizar
-                    la información
-                    de las declaraciones de situación patrimonial y de intereses de una manera uniforme e interoperable.
+                    La información de declaraciones de los servidores públicos de los diferentes niveles de gobierno se encuentra contenida en diversos formatos, bases de datos y sistemas de información; cada uno de ellos con particularidades tecnológicas y reglas de negocio distintas que complican la integración y la interoperabilidad de los datos. El Estándar para la Interoperabilidad de Datos de Declaraciones surge a partir de la necesidad de distribuir, comparar y analizar la información de las declaraciones de situación patrimonial y de intereses de una manera uniforme e interoperable.
                 </Typography>
 
                 <Typography paragraph color="textPrimary">
-                    Conforme a lo anterior, la SESNA ha conceptualizado a la Plataforma Digital Nacional como una
-                    herramienta que permitirá la consulta de información de las diferentes instituciones de los tres
-                    niveles de gobierno en un solo punto, sin tener el objetivo de concentrarla o resguardarla.
-                    En ese sentido, resulta necesario dotar a la PDN de mecanismos le que permitan interconectarse
-                    con los diversos sistemas de gobierno para consultar la información que resulte necesaria,
-                    sin importar la tecnología con la que dichos sistemas fueron desarrollados (i.e., lenguajes de programación, bases de datos, etc.).
+                    Conforme a lo anterior, la SESNA ha conceptualizado a la Plataforma Digital Nacional como una herramienta que permitirá la consulta de información de las diferentes instituciones de los tres niveles de gobierno, sin tener el objetivo de concentrarla o resguardarla. En ese sentido, resulta necesario dotar a la PDN de mecanismos que le permitan interconectarse con los diversos sistemas de gobierno para consultar la información que resulte necesaria, sin importar la tecnología con la que dichos sistemas fueron desarrollados (i.e. lenguajes de programación, bases de datos, etc.).
                 </Typography>
+
                 <Typography paragraph color="textPrimary">
-
-                    La PDN logrará la interoperabilidad técnica con los diversos sistemas que la integrarán a través
-                    de la creación de estándares de datos y mediante el uso de Interfaces de Programación de Aplicaciones
-                    o APIs (por sus siglas en Inglés). Los estándares de datos permitirán homologar la manera en que la
-                    información se debe representar para su entrega a la PDN, mientras que las APIs serán el mecanismo
-                    que permitirá la comunicación entre sistemas a través de Internet.
-                    Las APIs son ampliamente usadas para el desarrollo de aplicaciones a gran escala.
-                    El uso de APIs permitirá que las instituciones conserven el control de sus datos, gestionando el
-                    acceso a los mismos mediante reglas y perfiles de usuario.
-
+                    La PDN logrará la interoperabilidad técnica con los diversos sistemas que la integrarán a través de la creación de estándares de datos y mediante el uso de Interfaces de Programación de Aplicaciones (APIs, por sus siglas en Inglés). Los estándares de datos permitirán homologar la manera en que la información se debe representar para su entrega a la PDN, mientras que las APIs serán el mecanismo que permitirá la comunicación entre sistemas a través de Internet. Las APIs son ampliamente usadas para el desarrollo de aplicaciones a gran escala. El uso de APIs permitirá que las instituciones conserven el control de sus datos, gestionando el acceso a los mismos mediante reglas y perfiles de usuario.
                 </Typography>
+
+
                 <Typography paragraph color="textPrimary">
-                    La SESNA ha planeado el desarrollo de la PDN mediante el uso APIs Web
-                    con arquitectura REST (REpresentational State Transfer).
-                    Dicha tecnología es ampliamente usada por en la industria del software para el desarrollo de aplicaciones web,
-                    y en la actualidad, existe grán diversidad de herramientas de código abierto que permiten la implementación de
-                    sistemas basados en APIs REST de manera rápida y a un bajo costo.
+                    La SESNA ha planeado el desarrollo de la PDN mediante el uso APIs Web con arquitectura REST (REpresentational State Transfer). Dicha tecnología es ampliamente usada por en la industria del software para el desarrollo de aplicaciones web, y en la actualidad, existe grán diversidad de herramientas de código abierto que permiten la implementación de sistemas basados en APIs REST de manera rápida y a un bajo costo.
                 </Typography>
 
                 <Typography variant="h5" paragraph color="textPrimary">
