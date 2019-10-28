d12aea7f3121b3e624b0100a44d5404d27d8b512
Mario Torres <mario291187@gmail.com>
2019-10-28 17:19:56 -0600

    replacing @material-ui/styles -> @material-ui/core/styles

--- src/components/Home/Explora.js
+++ src/components/Home/Explora.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {withStyles} from '@material-ui/styles';
+import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import {Typography} from "@material-ui/core";
