9a3e4b8c3a2bc5e37ee5bc1373b86649c656452f
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2019-12-02 12:46:59 -0600

    validador para los sistemas s1, s2, s3

--- src/components/Validador/UploadForm.js
+++ src/components/Validador/UploadForm.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+/* import FormHelperText from '@material-ui/core/FormHelperText'; */
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import Ajv from 'ajv';
 import localize from 'ajv-i18n';
 //let SwaggerParser = require('swagger-parser');
@@ -15,65 +21,226 @@ const styles = theme => ({
         marginTop: theme.spacing(1),
         marginRight: theme.spacing(1),
         marginBottom: theme.spacing(2),
-        background: '#ffe01b',//'#fecb6e'
-    }
+        background: '#ffe01b',
+    },
+    button2: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(2),
+  },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+    },
 });
 
 class UploadForm extends React.Component {
     state = {
-        file: '',
-        disabled: false,
-        label: 'Cargar Archivo'
+      disabled: false,
+      label: 'Cargar Archivo',
+      isSubmitting: false,
+      formValues: {
+          uploadJson: "",
+          tipoSistema: ""
+      },
+        formErrors: {
+          uploadJson: "",
+          tipoSistema: ""
+      },
+        formValidity: {
+          uploadJson: false,
+          tipoSistema: false
+      },
     };
-// Handle Upload Using FileReader
-    handleFile = (e) => {
-        let reader = new FileReader();
-        let file = e.target.files[0];
-        let upload;
-        reader.onloadend = (event) => {
-          try {
-            this.setState({ file: event.target.result });
-            upload = JSON.parse(event.target.result);
-            this.setState({disabled: !this.state.disabled, label: 'Validando'});
-            fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
-                .then(res => res.json())
-                .then(results => {
-                    Parser.validate(results)
-                        .then((esquema) => {
-                            let ajv = new Ajv({ allErrors: true });
-                            let validate = ajv.compile(esquema.components.schemas.Declaraciones);
-                            let valid = validate(upload);
-                            this.setState({disabled: !this.state.disabled, label: 'Terminado'});
-                            if (valid) {
-                                // console.log('Valid!');
-                                this.props.onResults(valid)
-                            } else {
-                                // console.log('Invalid: ' + ajv.errorsText(validate.errors));
-                                localize.es(validate.errors)
-                                this.props.onResults(validate.errors)
-                            }
-                        })
-                        .catch(function(err) {
-                            this.props.onResults(err);
-                        });
-                      })
-          }
-          catch (e) {
-            this.props.onResults(e);
-          }
-        };
-        reader.readAsText(file);
-      };
 
+    handleValidation = target => {
+        const { name, value } = target;
+        const fieldValidationErrors = this.state.formErrors;
+        const validity = this.state.formValidity;
+        /* const isSistema = name === "tipoSistema";
+        const isUpload = name === "uploadJson"; */
+        validity[name] = value.length > 0 || value instanceof File;
+        fieldValidationErrors[name] = validity[name]
+          ? ""
+          : 'Campo requerido';
+    
+        /* if (validity[name]) {
+          if (isSistema) {
+            validity[name] = value.length >= 2;
+            fieldValidationErrors[name] = validity[name]
+              ? ""
+              : "Debes elegir un tipo de sistema";
+          }
+          if (isUpload) {
+            validity[name] = value instanceof File || value.length >= 3;
+            fieldValidationErrors[name] = validity[name]
+              ? ""
+              : "Debes cargar un archivo JSON";
+          }
+        } */
+        this.setState({
+          formErrors: fieldValidationErrors,
+          formValidity: validity
+        });
+      };
+       
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ isSubmitting: true });
+        const { formValues, formValidity } = this.state;
+        console.log(formValues)
+        if (Object.values(formValidity).every(Boolean)) {         
+          let file = this.state.formValues.uploadJson;
+          /* console.log(file) */
+          let reader = new FileReader();
+          reader.onloadend = (event) => {
+            let upload = JSON.parse(event.target.result)
+            try {
+              let tipoEsquema = this.state.formValues.tipoSistema
+              let urlTipoSistema;
+              let esquemaOAS;
+              switch (tipoEsquema) {
+                case 's1I':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/OAS_API_Declaraciones_Inicial.json';
+                  esquemaOAS = 'resDeclaraciones'
+                  break;
+                case 's1M':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/OAS_API_Declaraciones_Modificacion.json';
+                  esquemaOAS = 'resDeclaraciones'
+                  break;
+                case 's1C':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/OAS_API_Declaraciones_Conclusion.json';
+                  esquemaOAS = 'resDeclaraciones'
+                  break;
+                case 's2':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S2/oas/OAS_API_servidores_intervienen_contrataciones.json';
+                  esquemaOAS = 'resSpic'
+                  break;
+                case 's3P':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S3/oas/OAS_API_Particulares_Sancionados.json';
+                  esquemaOAS = 'resParticularesSancionados'
+                  break;
+                case 's3SP':
+                  urlTipoSistema = 'https://raw.githubusercontent.com/PDNMX/api_docs/master/S3/oas/OAS_API_Servidores_Sancionados.json';
+                  esquemaOAS = 'ssancionados'
+                  break;
+              }
+
+              this.setState({disabled: !this.state.disabled, label: 'Validando...'});
+              this.setState({ isSubmitting: true });
+              fetch(urlTipoSistema)
+                  .then(res => res.json())
+                  .then(results => {
+                      Parser.validate(results)
+                          .then((esquema) => {
+                              let ajv = new Ajv({ allErrors: true });
+                              ajv.addFormat("float","^[-+]?[0-9]*\\.?[0-9]+$");
+                              /* let pathFix = 'esquema.components.schemas.' + esquemaOAS; */
+                              console.log(esquema.components.schemas[esquemaOAS]); 
+                              let validate = ajv.compile(esquema.components.schemas[esquemaOAS]);
+                              let valid = validate(upload);
+                              console.log(valid)
+                              this.setState({disabled: !this.state.disabled, label: 'Terminado'});
+                              if (valid) {
+                                  // console.log('Valid!');
+                                  this.props.onResults(valid)
+                                  this.setState({ isSubmitting: false });
+                              } else {
+                                  // console.log('Invalid: ' + ajv.errorsText(validate.errors));
+                                  localize.es(validate.errors)
+                                  this.props.onResults(validate.errors)
+                                  this.setState({ isSubmitting: false });
+                              }
+                          })
+                          .catch(function(err) {
+                              console.log(err);
+                          });
+                        })
+            }
+            catch (e) {
+              this.props.onResults(e);
+            }
+          }
+          reader.readAsText(file);
+          this.setState({ isSubmitting: false });
+        } else {
+            for (let key in formValues) {
+              let target = {
+                name: key,
+                value: formValues[key]
+            };
+            /* console.log(target) */
+            this.handleValidation(target);
+          }
+          this.setState({ isSubmitting: false });
+        }
+    };
+
+    handleChange = ({ target }) => {
+        const { formValues } = this.state;
+        if(target.files !== undefined){
+            formValues[target.name] = target.files[0];
+        } else {
+            formValues[target.name] = target.value;
+        }
+        /* formValues[target.name] = target.value; */
+        this.setState({ formValues });
+        this.handleValidation(target);
+    };
+    
     render() {
         const {classes} = this.props;
+        const { formValues, formErrors, isSubmitting } = this.state;
         return (
             <div className={classes.root}>
-                <Button variant="contained" component="label" disabled={this.state.disabled} size='large' className={classes.button}>
-                    {this.state.label}
-                    <input onChange={this.handleFile} accept="application/json" type="file" style={{ display: "none" }}
-                    />
-                </Button>
+                <form onSubmit={this.handleSubmit}>
+                  <FormControl className={classes.formControl}>
+                    <Button variant="contained" component="label" disabled={this.state.disabled} size='large' className={classes.button2}>
+                      {/* <i className="material-icons">cloud_upload</i> */}
+                      Cargar Archivo
+                        <input name="uploadJson" onChange={this.handleChange} accept="application/json" type="file" style={{ display: "none" }}
+                        />
+                    </Button>
+                    <div className="invalid-feedback">{formErrors.uploadJson}</div>
+                  </FormControl>
+                  <FormControl className={classes.formControl}>
+                    <InputLabel id="demo-simple-select-required-label">Sistema</InputLabel>
+                    <Select
+                        /* labelId="demo-simple-select-required-label" */
+                        id="demo-simple-select-required"
+                        name="tipoSistema"
+                        value={formValues.tipoSistema}
+                        onChange={this.handleChange}
+                        className={classes.selectEmpty}
+                        >
+                        <MenuItem value="">
+                            <em>Ninguno</em>
+                        </MenuItem>
+                        <MenuItem value={'s1I'}>S1 - Inicial</MenuItem>
+                        <MenuItem value={'s1M'}>S1 - Modificación</MenuItem>
+                        <MenuItem value={'s1C'}>S1 - Conclusión</MenuItem>
+                        <MenuItem value={'s2'}>S2</MenuItem>
+                        <MenuItem value={'s3P'}>S3 - Particulares</MenuItem>
+                        <MenuItem value={'s3SP'}>S3 - Servidores Públicos</MenuItem>
+                    </Select>
+                    <div className="invalid-feedback">{formErrors.tipoSistema}</div>
+                    {/* <FormHelperText>Requerido</FormHelperText> */}
+                  </FormControl>
+                  <FormControl className={classes.formControl}>
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        className={classes.button}
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? "Validando..." : "Validar"}
+                    </Button>
+                  </FormControl>
+                    
+                </form>                
             </div>
         );
     }
--- src/components/Validador/index.js
+++ src/components/Validador/index.js
@@ -14,6 +14,7 @@ import PropTypes from 'prop-types';
 import Footer from "../Home/Footer";
 import Header from "./Header/Header";
 
+
 const styles = theme => ({
     root: {
         flexGrow: 1
@@ -87,6 +88,24 @@ class Validador extends Component {
                 <Header/>
                 <Grid container spacing={0} justify="center" className={classes.contents}>
                     <Grid item xs={12} className={classes.gridItem} align="center">
+                        <Typography paragraph color='textPrimary' align="left">
+                            Este validador te ayudará a verificar que la respuesta generada por tus API's cumplen las
+                            especificaciones que se refieren a los campos mínimos de datos que debe contener cada sistema, así como el estándar que debe seguir cada campo para ser interoperable con la Plataforma Digital Nacional.
+                            Dado que actualmente se encuentran disponibles las especificaciones para los Sistemas 1, 2 y 3, son estas las que se podrán verificar, para hacerlo sigue los siguientes pasos.
+                        </Typography>
+                        <Typography paragraph color='textPrimary' align="left">
+                            <ul>
+                                <li>Ejecuta tu API y guarda la respuesta en un archivo de texto con extensión</li>
+                                <li>Da clic en el botón "Cargar archivo" y selecciona el archivo que generaste.</li>
+                                <li>Selecciona el sistema cuya respuesta deseas validar.</li>
+                                <li>Da clic en el botón "Validar".</li>
+                            </ul>
+                        </Typography>
+                        
+                        <Typography paragraph color='textPrimary' align="left">
+                            En caso de que tu archivo sea validado exitosamente se desplegará un mensaje indicandolo.
+                            En caso de no validarse satisfactoriamente, se desplegará una tabla mostrando el(los) campo(s) que contiene(n) errores y el detalle de los mismos.
+                        </Typography>
 
                         <UploadForm onResults={this._handleResults}/>
                         <br/>
