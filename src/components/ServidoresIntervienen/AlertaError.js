96721fb3c34cc6f6301cbe61bf62d2d4f5f23e88
Mario Torres <mario291187@gmail.com>
2020-02-24 13:14:11 -0600

    Error Alerts

---
+++ src/components/ServidoresIntervienen/AlertaError.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
+
+export default function AlertaError(props) {
+    const classes = useStyles();
+    const {open, setOpen} = props;
+    const message = "Algo salio mal...";
+
+    return (
+        <div className={classes.root}>
+            <Collapse in={open}>
+                <Alert
+                    severity="error"
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                >
+                    <AlertTitle>Error</AlertTitle>
+                    {message}
+                </Alert>
+            </Collapse>
+        </div>
+    );
+}
