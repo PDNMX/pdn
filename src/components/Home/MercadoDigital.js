0941557c9ec118a360e584c959172a784f80dc53
Mario Torres <mario291187@gmail.com>
2020-09-22 22:49:15 -0500

    comentario

--- src/components/Home/MercadoDigital.js
+++ src/components/Home/MercadoDigital.js
@@ -3,7 +3,7 @@ import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import {Typography} from "@material-ui/core";
-import {Link} from "react-router-dom";
+//import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
