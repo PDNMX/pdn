a265fb123158b7158be14ca471b4c562f86933ce
Benito Perdomo <benito.perdomo.v@gmail.com>
2020-09-22 20:54:36 -0500

    refresh

--- src/components/Declaraciones2/ErrorBoundary.js
+++ src/components/Declaraciones2/ErrorBoundary.js
@@ -7,7 +7,7 @@ import styleSecciones from './styleSecciones';
 class ErrorBoundary extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { hasError: false, seccion: props.seccion };
+		this.state = { hasError: false, seccion: props.seccion, timer: null };
 	}
 
 	componentDidCatch(error, info) {
@@ -21,13 +21,24 @@ class ErrorBoundary extends React.Component {
 		// logErrorToMyService(error, info);
 	}
 
+	componentDidMount() {
+		let timer = setTimeout(() => {
+			window.location.reload();
+		}, 3000);
+		this.setState({ timer });
+	}
+	componentWillUnmount() {
+		let timer = this.state.timer;
+		clearTimeout(timer);
+	}
+
 	render() {
 		let { classes } = this.props;
 		if (this.state.hasError) {
 			return (
 				<Grid item xs={12} style={{ marginTop: 20 }}>
 					<Typography className={classes.alertWarning} align="center">
-						Algo ha salido mal... Por favor regresa al buscador.
+						Algo ha salido mal... espera unos segundos el navegador se refrescará en 3 segundos.
 					</Typography>
 				</Grid>
 			);
