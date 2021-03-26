beeb4ec6ac2aa039f03ab42497ec078417f211dc
Mario Torres <mario291187@gmail.com>
2021-03-25 18:10:58 -0600

    mda

--- src/components/Home/BarraFea.js
+++ src/components/Home/BarraFea.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
 import Blog from "../../assets/iconos_barra/ico-blog.svg";
 import Calidad from "../../assets/iconos_barra/ico-calidad_datos.svg";
 import Espe from "../../assets/iconos_barra/ico-especificaciones.svg";
 import Mesa from "../../assets/iconos_barra/ico-mesa_ayuda.svg";
-//import Plat from "../../assets/iconos_barra/ico-plataformas_sistemas.svg";
-import { Link } from "react-router-dom";
-import Typography from "@material-ui/core/Typography";
+import MDA from "../../assets/iconos_barra/mda.png";
+import { Link as RouterLink } from "react-router-dom";
+import {Typography, Link, Box} from "@material-ui/core";
 
 const styles = theme => ({
   root: {
@@ -16,14 +15,11 @@ const styles = theme => ({
     textAlign: "center"
   },
   icon: {
-    maxWidth: 60,
+    width: 60,
     paddingTop: 20,
     paddingBottom: 0,
     paddingRight: 50,
     paddingLeft: 50
-  },
-  innerContainer: {
-    maxWidth: 1200
   },
   link: {
     textDecoration: "none",
@@ -42,69 +38,61 @@ class BarraFea extends React.Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Grid
-          container
-          spacing={0}
-          justify="center"
-          className={classes.container}
-        >
-          <Grid
-            container
-            spacing={0}
-            justify="center"
-            className={classes.innerContainer}
-          >
-            <Grid item xs={6} md={3} className={classes.item}>
-              <Link to="/mesa-de-ayuda" className={classes.link}>
+        <Box display="flex" flexWrap="wrap" justifyContent="center">
+
+            <Box className={classes.item} flexGrow={1}>
+              <RouterLink to="/mesa-de-ayuda" className={classes.link}>
                 <img src={Mesa} className={classes.icon} alt="Mesa de ayuda" />
                 <Typography>Mesa de ayuda</Typography>
-              </Link>
-            </Grid>
-            <Grid item xs={6} md={3} className={classes.item}>
-              <a
+              </RouterLink>
+            </Box>
+
+            <Box className={classes.item} flexGrow={1}>
+              <Link
                 href="https://www.plataformadigitalnacional.org/blog"
                 className={classes.link}
+                underline="none"
               >
                 <img src={Blog} className={classes.icon} alt="Blog" />
                 <Typography>Blog</Typography>
-              </a>
-            </Grid>
+              </Link>
+            </Box>
 
-            <Grid item xs={6} md={3} className={classes.item}>
-              <Link to="/gaa/calidad" className={classes.link}>
+            <Box className={classes.item} flexGrow={1}>
+              <RouterLink to="/gaa/calidad" className={classes.link}>
                 <img
                   src={Calidad}
                   className={classes.icon}
                   alt="Calidad de datos"
                 />
                 <Typography>Calidad de datos</Typography>
-              </Link>
-            </Grid>
-            <Grid item xs={6} md={3} className={classes.item}>
-              <Link to="/especificaciones" className={classes.link}>
+              </RouterLink>
+            </Box>
+
+            <Box className={classes.item} flexGrow={1}>
+              <RouterLink to="/especificaciones" className={classes.link}>
                 <img
                   src={Espe}
                   className={classes.icon}
                   alt="Especificaciones técnicas"
                 />
                 <Typography>Especificaciones</Typography>
-              </Link>
-            </Grid>
+              </RouterLink>
+            </Box>
 
-            {/*
-            <Grid item xs={6} md={2} className={classes.item}>
-              <a href="https://plataformadigitalnacional.org/mapa-sla/" className={classes.link}>
+            <Box className={classes.item} flexGrow={1}>
+              <Link href="https://mda.plataformadigitalnacional.org/"
+                    underline="none"
+                    className={classes.link}>
                 <img
-                  src={Plat}
+                  src={MDA}
                   className={classes.icon}
-                  alt="Interconexión subnacional"
+                  alt="Mercado Digital Anticorrupción"
                 />
-                <Typography>Interconexión subnacional</Typography>
-              </a>
-            </Grid>
-            */}
-          </Grid>
-        </Grid>
+                <Typography>MDA</Typography>
+              </Link>
+            </Box>
+        </Box>
       </div>
     );
   }
