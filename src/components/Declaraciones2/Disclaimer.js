7454f193f465ac022ac086210f0fd44e19f3806e
Benito Perdomo <benito.perdomo.v@gmail.com>
2021-05-14 13:22:31 -0500

    update se activa y actualiza el disclaimer

--- src/components/Declaraciones2/Disclaimer.js
+++ src/components/Declaraciones2/Disclaimer.js
@@ -1,62 +1,58 @@
 import React from 'react';
-import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogContentText,
-    DialogActions,
-    Button
-} from "@material-ui/core";
-export default function AlertDialog(props) {
-    return (
-        <Dialog
-            open={props.open}
-            onClose={props.handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">
-                {"Plataforma Digital Nacional"}
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText
-                    id="alert-dialog-description"
-                    style={{ textAlign: "justify" }}
-                >
-                    La información contenida en esta sección{" "}
-                    <span
-                        style={{
-                            fontWeight: "bold",
-                            backgroundColor: "red",
-                            color: "#FFF",
-                            padding: "3px 10px"
-                        }}
-                    >
-                        NO ES REAL
-            </span>
-            , fue generada de forma aleatoria y sirve unicamente para poder
-            visualizar las diferentes funcionalidades propuestas para este
-            sistema.
-          </DialogContentText>
-                <DialogContentText style={{ textAlign: "justify" }}>
-                    <b>
-                        El formato actual está basado en la última versión de las
-                        especificaciones técnicas publicadas en este sitio, mismas que
-                        fueron elaboradas bajo los últimos formatos publicados en el Diario
-                        Oficial de la Federación.
-            </b>
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    variant="contained"
-                    onClick={props.handleClose}
-                    style={{ background: "#ffe01b" }}
-                >
-                    Aceptar
-          </Button>
-            </DialogActions>
-        </Dialog>
-    );
-}
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MuiLink from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
 
+const styles = (theme) => ({
+	button: {
+		background: '#ffe01b'
+	}
+});
+
+const useStyles = makeStyles(styles);
+
+export default function AlertDialog(props) {
+	const classes = useStyles();
+	return (
+		<Dialog
+			open={props.open}
+			onClose={props.handleClose}
+			aria-labelledby="alert-dialog-title"
+			aria-describedby="alert-dialog-description"
+		>
+			<DialogTitle id="alert-dialog-title">Plataforma Digital Nacional</DialogTitle>
+			<DialogContent>
+				<DialogContentText id="alert-dialog-description" style={{ textAlign: 'justify' }}>
+					La totalidad de la información que se muestra en este Sistema es responsabilidad exclusiva de quien
+					la genera, concentra y provee a esta Plataforma. Esto está establecido en las{' '}
+					<MuiLink
+						href="https://www.dof.gob.mx/nota_detalle.php?codigo=5541802&fecha=23/10/2018"
+						target="_blanck"
+					>
+						Bases para el Funcionamiento de la Plataforma Digital Nacional
+					</MuiLink>{' '}
+					(Artículos: 3, 6, 7 y 8 ) y la{' '}
+					<MuiLink href="http://www.diputados.gob.mx/LeyesBiblio/pdf/LGRA_130420.pdf" target="_blanck">
+						Ley General de Responsabilidades Administrativas
+					</MuiLink>{' '}
+					(Artículos: 31 y 32).
+				</DialogContentText>
+				<DialogContentText id="alert-dialog-description2" style={{ textAlign: 'justify' }}>
+					Te invitamos a revisar los{' '}
+					<MuiLink component={Link} to="/terminos">
+						"Términos y Condiciones de Uso"
+					</MuiLink>
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button variant="contained" component={Link} to="/terminos" className={classes.button}>
+					Términos
+				</Button>
+				<Button variant="contained" onClick={props.handleClose} className={classes.button}>
+					Aceptar
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+}
