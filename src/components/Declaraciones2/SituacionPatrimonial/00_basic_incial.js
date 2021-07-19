a276d9d2bda8a9fae5a0173cca33fa3a39dcfa69
Benito Perdomo <benito.perdomo.v@gmail.com>
2021-07-19 13:46:52 -0500

    ADD estructura basica  inicial, modificación

---
+++ src/components/Declaraciones2/SituacionPatrimonial/00_basic_incial.js
@@ -0,0 +1,151 @@
+export default {
+    datosGenerales: {
+        nombre: null,
+        primerApellido: null,
+        segundoApellido: null,
+        curp: null,
+        rfc: {
+            rfc: null,
+            homoClave: null,
+        },
+        correoElectronico: {
+            institucional: null,
+            personal: null,
+        },
+        telefono: {
+            casa: null,
+            celularPersonal: null,
+        },
+        situacionPersonalEstadoCivil: {
+            clave: null,
+            valor: null,
+        },
+        regimenMatrimonial: {
+            clave: null,
+            valor: null,
+        },
+        paisNacimiento: null,
+        nacionalidad: null,
+        aclaracionesObservaciones: null
+    },
+    ingresos: {
+        remuneracionMensualCargoPublico: {
+            valor: null,
+            moneda: null
+        },
+        otrosIngresosMensualesTotal: {
+            valor: null,
+            moneda: null
+        },
+        actividadIndustialComercialEmpresarial: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            actividades: []
+        },
+        actividadFinanciera: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            actividades: []
+        },
+        serviciosProfesionales: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            servicios: []
+        },
+        otrosIngresos: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            ingresos: []
+        },
+        ingresoMensualNetoDeclarante: {
+            valor: null,
+            moneda: null
+        },
+        ingresoMensualNetoParejaDependiente: {
+            valor: null,
+            moneda: null
+        },
+        totalIngresosMensualesNetos: {
+            valor: null,
+            moneda: null
+        },
+        aclaracionesObservaciones: null
+    },
+    actividadAnualAnterior: {
+        servidorPublicoAnioAnterior: null,
+        fechaIngreso: null,
+        fechaConclusion: null,
+        remuneracionNetaCargoPublico: {
+            valor: null,
+            moneda: null
+        },
+        otrosIngresosTotal: {
+            valor: null,
+            moneda: null,
+        },
+        actividadIndustialComercialEmpresarial: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null,
+            },
+            actividades: [
+            ]
+        },
+        actividadFinanciera: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            actividades: [
+            ]
+        },
+        serviciosProfesionales: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            servicios: [
+
+            ]
+        },
+        enajenacionBienes: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null,
+            },
+            bienes: [
+
+            ]
+        },
+        otrosIngresos: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            ingresos: [
+
+            ]
+        },
+        ingresoNetoAnualDeclarante: {
+            valor: null,
+            moneda: null,
+        },
+        ingresoNetoAnualParejaDependiente: {
+            valor: null,
+            moneda: null,
+        },
+        totalIngresosNetosAnuales: {
+            valor: null,
+            moneda: null
+        },
+        aclaracionesObservaciones: null
+    }
+}
---
+++ src/components/Declaraciones2/SituacionPatrimonial/00_basic_modif.js
@@ -0,0 +1,65 @@
+export default {
+    ingresos: {
+        remuneracionAnualCargoPublico: {
+            valor: null,
+            moneda: null
+        },
+        otrosIngresosAnualesTotal: {
+            valor: null,
+            moneda: null
+        },
+        actividadIndustialComercialEmpresarial: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            actividades: [
+            ]
+        },
+        actividadFinanciera: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            actividades: [
+            ]
+        },
+        serviciosProfesionales: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            servicios: [
+            ]
+        },
+        enajenacionBienes: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            bienes: [
+            ]
+        },
+        otrosIngresos: {
+            remuneracionTotal: {
+                valor: null,
+                moneda: null
+            },
+            ingresos: [
+            ]
+        },
+        ingresoAnualNetoDeclarante: {
+            valor: null,
+            moneda: null
+        },
+        ingresoAnualNetoParejaDependiente: {
+            valor: null,
+            moneda: null
+        },
+        totalIngresosAnualesNetos: {
+            valor: null,
+            moneda: null
+        },
+        aclaracionesObservaciones: null
+    },
+}
