0333cbbdee3e79f98a4d88df127d3255636e346a
Mario Torres <mario291187@gmail.com>
2021-09-08 10:55:09 -0500

    Disclaimer

--- src/components/Home/AlertDialog.js
+++ src/components/Home/AlertDialog.js
@@ -6,8 +6,8 @@ import DialogContent from '@material-ui/core/DialogContent';
 //import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {withStyles}from '@material-ui/core/styles';
-import {Link} from 'react-router-dom';
-import MuiLink from '@material-ui/core/Link';
+import {Link as RouterLink} from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import {Typography} from "@material-ui/core";
 
 const styles = theme => ({
@@ -28,7 +28,8 @@ const styles = theme => ({
             width: "1em",
             marginLeft: "-1em"
         },
-        padding: theme.spacing(1)
+        padding: theme.spacing(1),
+        marginTop: 0,
     }
 });
 
@@ -51,35 +52,44 @@ const AlertDialog = props => {
                 <DialogTitle id="alert-dialog-title">{"Plataforma Digital Nacional"}</DialogTitle>
                 <DialogContent>
 
-                    <Typography paragraph>
-                        Esta es la versión Beta 0.7 de la Plataforma Digital Nacional (PDN), lo cual significa:
+                    <Typography paragraph align="justify">
+                        Esta es la versión 1.0 de la Plataforma Digital Nacional (PDN), lo cual significa que:
                     </Typography>
 
                     <ul className={classes.ul}>
                         <li className={classes.li}>
                             <Typography color='textPrimary' display='inline'>
-                                <b>Beta:</b> Se encuentra en una versión preliminar, es decir, se están mejorando constantemente sus funcionalidades.
+                                Contiene datos reales de los Sistemas: 1, 2, 3, y 6.
                             </Typography>
                         </li>
                         <li className={classes.li}>
                             <Typography color='textPrimary' display='inline'>
-                                <b>0.7:</b> Contiene datos reales de los Sistemas 1, 2, 3, y 6 y se logró la interconexión con algunos de los sujetos obligados.
+                                Contiene nuevas secciones y funcionalidades.
+                            </Typography>
+                        </li>
+                        <li className={classes.li}>
+                            <Typography color='textPrimary' display="inline">
+                                La Plataforma se construye de manera modular y escalable. Como lo establecen
+                                las <Link href="https://www.dof.gob.mx/nota_detalle.php?codigo=5541802&fecha=23/10/2018" target="_blank">
+                                Bases para el Funcionamiento de la Plataforma Digital Nacional
+                            </Link> en
+                                el Capítulo IX, la Plataforma tendrá constantes actualizaciones y mejoras, por lo que esta versión NO debe ser vista como la última.
                             </Typography>
                         </li>
                     </ul>
 
-                    <Typography paragraph>
-                        Es decir, esta versión  <b>NO debe ser vista como final</b>.
+                    <Typography paragraph align="justify">
+                        Todos los usuarios de la PDN deberán privilegiar los intereses de las personas titulares de los datos personales contenidos en los sistemas. El tratamiento de los datos consultables en la PDN no debe utilizarse para ningún tipo de discriminación, trato injusto, arbitrario o contrario a lo establecido en la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados.
                     </Typography>
 
-                    <Typography paragraph>
-                        Te invitamos a revisar los <MuiLink component={Link} to='/terminos'>"Términos y Condiciones de Uso"</MuiLink>,
+                    <Typography paragraph align="justify">
+                        Te invitamos a revisar los <Link component={RouterLink} to='/terminos'>"Términos y Condiciones de Uso"</Link>,
                         así como a dejar tus comentarios sobre las funcionalidades y experiencia de usuario en la sección de "Comenta".
                     </Typography>
 
                 </DialogContent>
                 <DialogActions>
-                    <Button variant="contained" component={Link} to='/terminos' className={classes.button}>
+                    <Button variant="contained" component={RouterLink} to='/terminos' className={classes.button}>
                         Términos
                     </Button>
                     <Button variant="contained" onClick={handleClose} className={classes.button}>
