920b7e1f6ca00d20748ef30ab11e22c879c1fae5
rosa isela morquecho <isela.escom@gmail.com>
2022-02-24 11:24:06 -0600

    change Previos to functional component

--- src/components/Compartidos/Previos.js
+++ src/components/Compartidos/Previos.js
@@ -15,7 +15,6 @@ import TablePagination from '@mui/material/TablePagination';
 import TableFooter from "@mui/material/TableFooter";
 
 const styles = theme => ({
-
     table: {
         tableLayout: 'fixed',
     },
@@ -28,128 +27,121 @@ const styles = theme => ({
         marginBottom: '30px',
         overflowX: 'auto',
     },
-    iconoVer:{
+    iconoVer: {
         color: theme.palette.secundario.dark,
         cursor: 'pointer'
     },
     tableHead: {
         color: theme.palette.text.main
     },
-    tablePagination:{
-        color : theme.palette.greyColor,
-        backgroundColor : '#f2f2f2'
+    tablePagination: {
+        color: theme.palette.greyColor,
+        backgroundColor: '#f2f2f2'
     }
 });
 
-class Previos extends React.Component {
+function Previos({data, classes}) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            page: 0,
-            rowsPerPage: 5
-        };
-    }
+    let [page, setPage] = React.useState(0);
+    let [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-
-    handleChangePage = (event, newPage) => {
-        this.setState({
-            page : newPage
-        });
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage)
     };
-    handleChangeRowsPerPage = (event) => {
-        this.setState({
-            rowsPerPage : parseInt(event.target.value, 10),
-            page : 0
-        });
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
     };
-    render() {
-        let {data,classes} = this.props;
-        let {rowsPerPage, page} = this.state;
-        return (
-            <div>
-                <Grid container justifyContent='center' spacing={0} className={classes.gridTable}>
-                    <Grid item xs={12}>
-                        {data && data.length > 0 &&
-                        <Typography variant="body1" >La siguiente tabla muestra el resultado devuelto por cada Proveedor de información a través de su API. Pulsa en <IconSubdirectory className={classes.iconoVer}/> para ver el detalle<br/></Typography>
-                        }
-                    </Grid>
-                    <Grid item xs={12}>
-                        {data && data.length > 0 &&
-                        <div className={classes.container}>
-                            <Table className={classes.table}>
-                                <TableHead style={{backgroundColor:'#0d3b49'}}>
-                                    <TableRow>
-                                        <TableCell align="left" variant={"head"} className={classes.tableHead}><Typography variant={"body1"}>Nivel</Typography></TableCell>
-                                        <TableCell align="left" variant={"head"} className={classes.tableHead}><Typography variant={"body1"}> Proveedor de información</Typography></TableCell>
-                                        <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography variant={"body1"}>Estatus</Typography></TableCell>
-                                        <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography variant={"body1"}>Número de registros</Typography></TableCell>
-                                        <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography variant={"body1"}>Acciones</Typography></TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody style={{backgroundColor:'#f2f2f2'}}>
-                                    {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        .map(row => (
+
+    return (
+        <div>
+            <Grid container justifyContent='center' spacing={0} className={classes.gridTable}>
+                <Grid item xs={12}>
+                    {data && data.length > 0 &&
+                    <Typography variant="body1">La siguiente tabla muestra el resultado devuelto por cada Proveedor de
+                        información a través de su API. Pulsa en <IconSubdirectory className={classes.iconoVer}/> para
+                        ver el detalle<br/></Typography>
+                    }
+                </Grid>
+                <Grid item xs={12}>
+                    {data && data.length > 0 &&
+                    <div className={classes.container}>
+                        <Table className={classes.table}>
+                            <TableHead style={{backgroundColor: '#0d3b49'}}>
+                                <TableRow>
+                                    <TableCell align="left" variant={"head"} className={classes.tableHead}><Typography
+                                        variant={"body1"}>Nivel</Typography></TableCell>
+                                    <TableCell align="left" variant={"head"} className={classes.tableHead}><Typography
+                                        variant={"body1"}> Proveedor de información</Typography></TableCell>
+                                    <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography
+                                        variant={"body1"}>Estatus</Typography></TableCell>
+                                    <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography
+                                        variant={"body1"}>Número de registros</Typography></TableCell>
+                                    <TableCell align="center" variant={"head"} className={classes.tableHead}><Typography
+                                        variant={"body1"}>Acciones</Typography></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody style={{backgroundColor: '#f2f2f2'}}>
+                                {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map(row => (
                                         <TableRow key={row.supplier_id}>
-                                            <TableCell align="left">{row.levels? row.levels.join(','):''}</TableCell>
+                                            <TableCell align="left">{row.levels ? row.levels.join(',') : ''}</TableCell>
                                             <TableCell align="left">{row.supplier_name}</TableCell>
                                             <TableCell align="center">
-                                                <Tooltip title={!row.error ? "Disponible": "No disponible"}>
-                                                    <IconSunny color={!row.error ? "primario":"disabled"}/>
+                                                <Tooltip title={!row.error ? "Disponible" : "No disponible"}>
+                                                    <IconSunny color={!row.error ? "primario" : "disabled"}/>
                                                 </Tooltip>
                                             </TableCell>
                                             <TableCell align="center">
-                                                {("undefined"=== typeof (row["totalRows"])) &&
+                                                {("undefined" === typeof (row["totalRows"])) &&
                                                 "No disponible"
                                                 }
                                                 {row.totalRows}
                                             </TableCell>
                                             <TableCell align="center">
-                                                {row.totalRows>0 &&
+                                                {row.totalRows > 0 &&
                                                 <Tooltip title={"Ver"}>
-                                                    <IconSubdirectory className={classes.iconoVer} onClick={()=>{
+                                                    <IconSubdirectory className={classes.iconoVer} onClick={() => {
                                                         this.props.handleChangeSujetoObligado(row.supplier_id);
                                                     }}/>
                                                 </Tooltip>
                                                 }
-                                                {("undefined"=== typeof (row["totalRows"])) &&
+                                                {("undefined" === typeof (row["totalRows"])) &&
                                                 "No disponible"
                                                 }
-                                                {(row.totalRows===0) &&
+                                                {(row.totalRows === 0) &&
                                                 "-"
                                                 }
                                             </TableCell>
                                         </TableRow>
                                     ))}
-                                </TableBody>
-                                <TableFooter>
-                                    <TableRow >
-                                        <TablePagination
-                                            className={classes.tablePagination}
-                                            colSpan = {5}
-                                            rowsPerPageOptions={[5,10,15]}
-                                            count = {data.length}
-                                            rowsPerPage = {rowsPerPage}
-                                            page = {page}
-                                            onPageChange={this.handleChangePage}
-                                            onRowsPerPageChange={this.handleChangeRowsPerPage}
-                                            labelRowsPerPage='Registros por página'
-                                            labelDisplayedRows={({from, to, count}) => {
-                                                return `${from}-${to} de ${count}`;
-                                            }}
-                                        />
-                                    </TableRow>
-                                </TableFooter>
-                            </Table>
-                        </div>
-                        }
-
-                    </Grid>
-
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TablePagination
+                                        className={classes.tablePagination}
+                                        colSpan={5}
+                                        rowsPerPageOptions={[5, 10, 15]}
+                                        count={data.length}
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onPageChange={handleChangePage}
+                                        onRowsPerPageChange={handleChangeRowsPerPage}
+                                        labelRowsPerPage='Registros por página'
+                                        labelDisplayedRows={({from, to, count}) => {
+                                            return `${from}-${to} de ${count}`;
+                                        }}
+                                    />
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                    </div>
+                    }
                 </Grid>
-            </div>
-        );
-    }
+            </Grid>
+        </div>
+    );
+
 
 }
 
