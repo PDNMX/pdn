07a7138556cf03f97dd955041d455f692d96de5d
Mario Torres <mario291187@gmail.com>
2022-03-09 12:56:51 -0600

    Paddings

--- src/components/Sistema4/index.js
+++ src/components/Sistema4/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography, Grid} from "@mui/material";
+import {Typography, Grid, Paper, Box} from "@mui/material";
 import {withStyles} from "@mui/styles";
 import HeaderV2 from '../HomeV2/HeaderV2';
 import bgimg from "../../assets/rediseno/fondo_cruces.png";
@@ -9,13 +9,29 @@ const styles = theme => ({
     root: {
         flexGrow :1,
         backgroundImage: `url(${bgimg})`,
-        backgroundRepeat: "repeat"
+        backgroundRepeat: "repeat",
+        backgroundPosition: 'fixed',
+        color: '#f2f2f2'
     },
-    rootItem: {
+    item: {
         maxWidth: 1200,
         padding: theme.spacing(1),
         paddingTop: 90,
         paddingBottom: 90,
+    },
+    paper: {
+        backgroundColor: theme.palette.background.opaque,
+        padding: theme.spacing(2),
+        color: theme.palette.primario.contrastText,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: theme.palette.secundario.main,
+        borderRadius: '10px 10px 10px 10px',
+        display: 'flex',
+        justifyContent: "center"
+    },
+    box: {
+        maxWidth: '900px', paddingTop: '50px', paddingBottom: '50px'
     }
 });
 
@@ -33,19 +49,23 @@ const index = props => {
     return <div className={classes.root}>
         <HeaderV2 section = {system}/>
         <Grid container justifyContent="center">
-            <Grid item xs={12} className={classes.rootItem}>
-                <CustomTypography variant="h4" paragraph>
-                    Sistema 4
-                </CustomTypography>
-                <CustomTypography>
-                    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-                </CustomTypography>
-                <CustomTypography>
-                    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-                </CustomTypography>
-                <CustomTypography>
-                    "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-                </CustomTypography>
+            <Grid item xs={12} className={classes.item}>
+                <Paper elevation={15} className={classes.paper}>
+                    <Box className={classes.box}>
+                        <CustomTypography variant="h4" paragraph>
+                            Sistema 4
+                        </CustomTypography>
+                        <CustomTypography>
+                            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+                        </CustomTypography>
+                        <CustomTypography>
+                            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+                        </CustomTypography>
+                        <CustomTypography>
+                            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+                        </CustomTypography>
+                    </Box>
+                </Paper>
             </Grid>
         </Grid>
     </div>
--- src/components/Sistema5/index.js
+++ src/components/Sistema5/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography, Grid} from "@mui/material";
+import {Typography, Grid, Paper, Box} from "@mui/material";
 import {withStyles} from "@mui/styles";
 import bgimg from "../../assets/rediseno/fondo_cruces.png";
 import HeaderV2 from '../HomeV2/HeaderV2';
@@ -10,12 +10,28 @@ const styles = theme => ({
         flexGrow :1,
         backgroundImage: `url(${bgimg})`,
         backgroundRepeat: "repeat",
+        backgroundPosition: 'fixed',
+        color: '#f2f2f2'
     },
-    rootItem: {
+    item: {
         maxWidth: 1200,
         padding: theme.spacing(1),
         paddingTop: 90,
         paddingBottom: 90,
+    },
+    paper: {
+        backgroundColor: theme.palette.background.opaque,
+        padding: theme.spacing(2),
+        color: theme.palette.primario.contrastText,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: theme.palette.secundario.main,
+        borderRadius: '10px 10px 10px 10px',
+        display: 'flex',
+        justifyContent: "center"
+    },
+    box: {
+        maxWidth: '900px', paddingTop: '50px', paddingBottom: '50px'
     }
 });
 
@@ -33,7 +49,9 @@ const index = props => {
     return <div className={classes.root}>
         <HeaderV2 section = {system}/>
         <Grid container justifyContent="center">
-            <Grid item xs={12} className={classes.rootItem}>
+            <Grid item xs={12} className={classes.item}>
+                <Paper elevation={15} className={classes.paper}>
+                    <Box className={classes.box}>
                 <CustomTypography variant="h4" paragraph>
                     Sistema 5
                 </CustomTypography>
@@ -47,6 +65,8 @@ const index = props => {
                 <CustomTypography>
                     "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
                 </CustomTypography>
+                    </Box>
+                </Paper>
             </Grid>
         </Grid>
     </div>
