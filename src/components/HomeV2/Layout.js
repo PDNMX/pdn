bee6b0ae0edd7a91f8308dd2af5010127b465c30
IselaMorquecho <isela.escom@gmail.com>
2022-03-14 12:50:49 -0600

    Chatito

--- src/components/HomeV2/Layout.js
+++ src/components/HomeV2/Layout.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Banner from './Banner';
 import Version from "./Version";
 import Footer from "./Footer/";
+import ChatBotPDN from "../ChatBot/ChatBotPDN";
+
 const Layout = ({children})=>{
     return (
         <React.Fragment>
             <Banner/>
             <Version/>
             {children}
+            <ChatBotPDN/>
             <Footer/>
         </React.Fragment>
     );
