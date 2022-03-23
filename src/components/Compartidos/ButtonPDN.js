68506d410fba274a0342d61207cd0d8e525172f1
Benito Perdomo <benito.perdomo.v@gmail.com>
2022-03-23 09:28:56 -0600

    UPDATE implement ButtonPDN

--- src/components/Compartidos/ButtonPDN.js
+++ src/components/Compartidos/ButtonPDN.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import withStyles from '@mui/styles/withStyles';
-import {Button} from "@mui/material";
+import { Button } from '@mui/material';
 
 const styles = theme => ({
-    btnPDN:{
-        margin: theme.spacing(1),
-        background: "rgb(255,255,255,0.5)",
-        borderRadius: "50px",
-        fontWeight: "bold",
-        fontStyle: "italic",
-        '&:hover': {
-            backgroundColor: "#56a3bf",
-        },
+  btnPDN: {
+    'margin': 0,
+    'background': 'rgb(255,255,255,0.5)',
+    'borderRadius': '50px',
+    'fontWeight': 'bold',
+    'fontStyle': 'italic',
+    '&:hover': {
+      backgroundColor: '#56a3bf'
     }
+  },
+  cover: {
+    margin: theme.spacing(1),
+    backgroundColor: '#124759',
+    opacity: 0.85,
+    borderRadius: '50px',
+    border: 0,
+    padding: 0
+  }
 });
 
-const ButtonPDN = (props) => {
-    const {classes} = props;
-    return (
-        <Button {...props} variant={'contained'} className={classes.btnPDN} />
-        )
-
-}
+const ButtonPDN = props => {
+  const { classes } = props;
+  return (
+    <span className={classes.cover}>
+      <Button {...props} variant={'contained'} className={classes.btnPDN} />
+    </span>
+  );
+};
 
 export default withStyles(styles)(ButtonPDN);
