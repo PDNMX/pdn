35907dbb581f667ad77765061468786cb239fb4d
IselaMorquecho <isela.escom@gmail.com>
2022-03-24 11:54:34 -0600

    ButtonPDN backgroundColor

--- src/components/Compartidos/ButtonPDN.js
+++ src/components/Compartidos/ButtonPDN.js
@@ -4,31 +4,21 @@ import { Button } from '@mui/material';
 
 const styles = theme => ({
   btnPDN: {
-    'margin': 0,
-    'background': 'rgb(255,255,255,0.5)',
+    'margin': theme.spacing(1),
+    'background': '#9eb1b6',
     'borderRadius': '50px',
     'fontWeight': 'bold',
     'fontStyle': 'italic',
     '&:hover': {
       backgroundColor: '#56a3bf'
     }
-  },
-  cover: {
-    margin: theme.spacing(1),
-    backgroundColor: '#124759',
-    opacity: 0.85,
-    borderRadius: '50px',
-    border: 0,
-    padding: 0
   }
 });
 
 const ButtonPDN = props => {
   const { classes } = props;
   return (
-    <span className={classes.cover}>
       <Button {...props} variant={'contained'} className={classes.btnPDN} />
-    </span>
   );
 };
 
