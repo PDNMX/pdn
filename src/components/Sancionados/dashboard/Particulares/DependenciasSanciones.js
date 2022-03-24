fe5fe427b64f4cc5b23f4e47f6755d25a4966ac4
IselaMorquecho <isela.escom@gmail.com>
2022-03-24 13:12:44 -0600

    Dashboards s3 functional components

--- src/components/Sancionados/dashboard/Particulares/DependenciasSanciones.js
+++ src/components/Sancionados/dashboard/Particulares/DependenciasSanciones.js
@@ -30,7 +30,6 @@ const styles = theme => ({
     }
 });
 
-
 function aux() {
     return new Promise((resolve, reject) => {
         let options = {
@@ -46,7 +45,6 @@ function aux() {
         });
     });
 }
-
 
 function loadData2() {
     return new Promise((resolve, reject) => {
@@ -71,149 +69,145 @@ let z = d3.scaleOrdinal()
         "#FF5722", "#795548", "#9E9E9E", "#607D8B"]);
 
 
-class DependenciasSanciones extends React.Component {
-    state = {
-        errorG1: false,
-        errorG2: false,
-    };
+const DependenciasSanciones = (props) => {
+    const [errorG1, setErrorG1] = React.useState(false);
+    const [errorG2, setErrorG2] = React.useState(false);
+    const [methods, setMethods] = React.useState({});
+    const [config2, setConfig2] = React.useState({});
 
-    componentDidMount() {
+    const {classes} = props;
+    React.useEffect(() => {
         aux().then(result => {
             let aux = result.data.data.map(item => {
                 return {
-                    "value": parseInt(item.total,10),
+                    "value": parseInt(item.total, 10),
                     "group": item.dependencia
                 }
             });
-            this.setState({
-                    methods: {
-                        data: aux,
-                        height: 400,
-                        groupBy: ["group"],
-                        sum: "value",
-                        tooltipConfig: {
-                            tbody: [
-                                ["Número de sanciones: ", function (d) {
-                                    return d["value"]
-                                }
-                                ]
-                            ]
-                        },
-                        legend: false,
-                        shapeConfig: {
-                            label: function (d) {
-                                return d["group"] + "\n" + d["value"] + " sanciones"
-                            },
-                            labelConfig: {
-                                fontMax: 18,
-                                fontMin: 10
-                            },
-                            fill: (d) => {
-                                return z(d.group)
-                            }
-                        },
-
+            setMethods({
+                data: aux,
+                height: 400,
+                groupBy: ["group"],
+                sum: "value",
+                tooltipConfig: {
+                    tbody: [
+                        ["Número de sanciones: ", function (d) {
+                            return d["value"]
+                        }
+                        ]
+                    ]
+                },
+                legend: false,
+                shapeConfig: {
+                    label: function (d) {
+                        return d["group"] + "\n" + d["value"] + " sanciones"
+                    },
+                    labelConfig: {
+                        fontMax: 18,
+                        fontMin: 10
+                    },
+                    fill: (d) => {
+                        return z(d.group)
                     }
-                }
-            )
+                },
+
+            })
+
         }).catch(err => {
             console.error(err);
-            this.setState({errorG1: true})
+            setErrorG1(true);
         });
 
         loadData2().then(result2 => {
             let aux2 = result2.data.data.map(item => {
                 return {
-                    "value": parseInt(item.total,10),
+                    "value": parseInt(item.total, 10),
                     "group": item.dependencia,
                     "parent": item.anio
                 }
             });
-            this.setState({
-
-                    config2: {
-                        data: aux2,
-                        height: 400,
-                        groupBy: ["parent", "group"],
-                        sum: "value",
-                        tooltipConfig: {
-                            tbody: [
-                                ["Número de sanciones: ", function (d) {
-                                    return d["value"]
-                                }
-                                ]
-                            ]
-                        },
-                        legend: true,
-                        shapeConfig: {
-                            label: function (d) {
-                                return d["group"] + "\n" + d["value"] + " sanciones"
-                            },
-                            labelConfig: {
-                                fontMax: 18,
-                                fontMin: 10
-                            },
-                            fill: (d) => {
-                                return z(d.parent)
-                            }
-                        },
+            setConfig2({
+                data: aux2,
+                height: 400,
+                groupBy: ["parent", "group"],
+                sum: "value",
+                tooltipConfig: {
+                    tbody: [
+                        ["Número de sanciones: ", function (d) {
+                            return d["value"]
+                        }
+                        ]
+                    ]
+                },
+                legend: true,
+                shapeConfig: {
+                    label: function (d) {
+                        return d["group"] + "\n" + d["value"] + " sanciones"
+                    },
+                    labelConfig: {
+                        fontMax: 18,
+                        fontMin: 10
+                    },
+                    fill: (d) => {
+                        return z(d.parent)
                     }
-                }
-            )
+                },
+            })
         }).catch(err => {
             console.error(err);
-            this.setState({errorG2: true})
+            setErrorG2(true);
         });
-    }
+    }, []);
 
-    render() {
-        const {classes} = this.props;
-        return (
-            <div>
-                <Grid container spacing={0} justifyContent='center' className={classes.frameChart}>
-                    <Grid item xs={12}>
-                        <Typography variant={"h6"} className={classes.titulo}>
-                            <b>{"Dependencias con mayor número de sanciones"}</b>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} className={classes.descripcion}>
-                        <Typography variant={"body1"}>
-                            Con respecto a las dependencias que cuentan con más sanciones a particulares , el Instituto Mexicano del Seguro Social es
-                            la que cuenta con el mayor número de sanciones con un total de 343 o bien el <b>16.53%</b>, seguida  por la Secretaría de la
-                            Función Pública con 272  que representa el <b>13.11%</b>, la Comisión Federal de Electricidad con 168 equivalente al <b>8.05%</b>.
-                            <br/>El resto de las dependencias en el top, presentan un total de sanciones menor a 100.
-                        </Typography>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        {
-                            this.state.methods && this.state.methods.data &&
-                            <Treemap config={this.state.methods}/>
-                        }
-                        {
-                            this.state.errorG1 && <MensajeErrorDatos/>
-                        }
-
-                    </Grid>
-                    <Grid item xs={12} >
-                        <Typography variant={"body1"} paragraph>
-                            En la siguiente gráfica, se puede apreciar el detalle por año, del 2004 a enero 2022.
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        {
-                            this.state.config2 && this.state.config2.data &&
-                            <Treemap config={this.state.config2}/>
-                        }
-                        {
-                            this.state.errorG2 && <MensajeErrorDatos/>
-                        }
-                    </Grid>
+    return (
+        <div>
+            <Grid container spacing={0} justifyContent='center' className={classes.frameChart}>
+                <Grid item xs={12}>
+                    <Typography variant={"h6"} className={classes.titulo}>
+                        <b>{"Dependencias con mayor número de sanciones"}</b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} className={classes.descripcion}>
+                    <Typography variant={"body1"}>
+                        Con respecto a las dependencias que cuentan con más sanciones a particulares , el Instituto
+                        Mexicano del Seguro Social es
+                        la que cuenta con el mayor número de sanciones con un total de 343 o bien el <b>16.53%</b>,
+                        seguida por la Secretaría de la
+                        Función Pública con 272 que representa el <b>13.11%</b>, la Comisión Federal de Electricidad con
+                        168 equivalente al <b>8.05%</b>.
+                        <br/>El resto de las dependencias en el top, presentan un total de sanciones menor a 100.
+                    </Typography>
                 </Grid>
 
-            </div>
-        )
-    }
+                <Grid item xs={12}>
+                    {
+                        methods && methods.data &&
+                        <Treemap config={methods}/>
+                    }
+                    {
+                        errorG1 && <MensajeErrorDatos/>
+                    }
+
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant={"body1"} paragraph>
+                        En la siguiente gráfica, se puede apreciar el detalle por año, del 2004 a enero 2022.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    {
+                        config2 && config2.data &&
+                        <Treemap config={config2}/>
+                    }
+                    {
+                        errorG2 && <MensajeErrorDatos/>
+                    }
+                </Grid>
+            </Grid>
+
+        </div>
+    )
+
 
 }
 
--- src/components/Sancionados/dashboard/Particulares/SentidoResoluciones.js
+++ src/components/Sancionados/dashboard/Particulares/SentidoResoluciones.js
@@ -5,10 +5,8 @@ import {Grid, Typography} from "@mui/material";
 import {BarChart} from "d3plus-react";
 import axios from 'axios';
 import 'react-vis/dist/style.css';
-// import VerticalBarSeries from "react-vis/es/plot/series/vertical-bar-series";
 import * as d3 from "d3";
 import MensajeErrorDatos from "../../../Mensajes/MensajeErrorDatos";
-
 
 const styles = theme => ({
     frameChart: {
@@ -49,7 +47,6 @@ function aux() {
     });
 }
 
-
 function loadData2() {
     return new Promise((resolve, reject) => {
         let options = {
@@ -71,178 +68,168 @@ let z = d3.scaleOrdinal()
     .range(["#2196F3", "#9C27B0", "#009688", "#FF9800", "#E91E63", "#FF5722"]);
 z.domain(["SANCIONATORIA CON MULTA E INHABILITACIÓN", "SANCIONATORIA CON MULTA", "SANCIONATORIA", "ABSOLUTORIA", "NO ESPECIFICA", "ABSOLUTORIA"])
 
-class SentidoResoluciones extends React.Component {
-    state = {
-        errorG1: false,
-        errorG2: false
-    };
+const SentidoResoluciones = (props) => {
+    const [errorG1, setErrorG1] = React.useState(false);
+    const [errorG2, setErrorG2] = React.useState(false);
+    const [methods, setMethods] = React.useState({});
+    const [config2, setConfig2] = React.useState({});
+    const {classes} = props;
 
-    componentDidMount() {
+    React.useEffect(() => {
         aux().then(result => {
             let aux = result.data.data.map(item => {
                 return {
                     "x": item.sentido_de_resolucion ? item.sentido_de_resolucion : "NO ESPECIFICA",
-                    "y": parseInt(item.total,10)
+                    "y": parseInt(item.total, 10)
                 }
             });
-            this.setState({
-                    methods: {
-                        data: aux,
-                        groupBy: "x",
-                        x: "x",
-                        y: "y",
-                        xConfig: {
-                            title: "Sentido de la resolución de la sanción",
-                        },
-                        yConfig: {
-                            title: "Número de sanciones",
+            setMethods({
+                data: aux,
+                groupBy: "x",
+                x: "x",
+                y: "y",
+                xConfig: {
+                    title: "Sentido de la resolución de la sanción",
+                },
+                yConfig: {
+                    title: "Número de sanciones",
 
-                        },
-                        tooltipConfig: {
-                            title: function (d) {
-                                return "Datos";
-                            },
-                            tbody: [
-                                ["Resolución: ", function (d) {
-                                    return d["x"]
-                                }
-                                ],
-                                ["Número de resoluciones: ", function (d) {
-                                    return d["y"]
-                                }
-                                ]
-                            ]
-                        },
-                        legend: false,
-                        height: 400,
-                        shapeConfig: {
-                            label: false,
-                            fill: (d, i) => {
-                                return color[i]
-                            }
-                        },
-                        title: "Historico",
+                },
+                tooltipConfig: {
+                    title: function (d) {
+                        return "Datos";
                     },
-
-
-                }
-            )
+                    tbody: [
+                        ["Resolución: ", function (d) {
+                            return d["x"]
+                        }
+                        ],
+                        ["Número de resoluciones: ", function (d) {
+                            return d["y"]
+                        }
+                        ]
+                    ]
+                },
+                legend: false,
+                height: 400,
+                shapeConfig: {
+                    label: false,
+                    fill: (d, i) => {
+                        return color[i]
+                    }
+                },
+                title: "Historico",
+            })
         }).catch(err => {
             console.error(err);
-            this.setState({errorG1: true})
+            setErrorG1(true);
         });
 
         loadData2().then(temp2 => {
             let aux2 = temp2.data.data.map(item => {
                 return {
                     id: item.sentido_de_resolucion,
-                    y: parseInt(item.total,10),
+                    y: parseInt(item.total, 10),
                     x: item.anio,
                 }
             });
-            this.setState({
-                    config2: {
-                        data: aux2,
-                        xConfig: {
-                            title: "Sentido de la resolución de la sanción",
-                        },
-                        yConfig: {
-                            title: "Número de sanciones"
-                        },
-                        tooltipConfig: {
-                            title: function (d) {
-                                return "Datos";
-                            },
-                            tbody: [
-                                ["Resolución: ", function (d) {
-                                    return d["id"]
-                                }
-                                ],
-                                ["Número de resoluciones: ", function (d) {
-                                    return d["y"]
-                                }
-                                ]
-                            ]
-                        },
-                        legend: true,
-                        height: 400,
-                        shapeConfig: {
-                            label: false,
-                            fill: (d) => {
-                                return z(d.id)
-                            }
-                        },
-                        stacked: true,
-                        title: "Por año",
+            setConfig2({
+                data: aux2,
+                xConfig: {
+                    title: "Sentido de la resolución de la sanción",
+                },
+                yConfig: {
+                    title: "Número de sanciones"
+                },
+                tooltipConfig: {
+                    title: function (d) {
+                        return "Datos";
                     },
+                    tbody: [
+                        ["Resolución: ", function (d) {
+                            return d["id"]
+                        }
+                        ],
+                        ["Número de resoluciones: ", function (d) {
+                            return d["y"]
+                        }
+                        ]
+                    ]
+                },
+                legend: true,
+                height: 400,
+                shapeConfig: {
+                    label: false,
+                    fill: (d) => {
+                        return z(d.id)
+                    }
+                },
+                stacked: true,
+                title: "Por año",
+            })
 
-                }
-            )
         }).catch(err => {
             console.error(err);
-            this.setState({errorG2: true})
+            setErrorG2(true);
         });
 
-    }
+    }, []);
 
-    render() {
-        const {classes} = this.props;
-        return (
-            <div>
-                <Grid container spacing={0} justifyContent='center' className={classes.frameChart}>
-                    <Grid item xs={12}>
-                        <Typography variant={"h6"} className={classes.titulo}>
-                            <b> Sentido de las resoluciones</b>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={8} className={classes.graph}>
-                        {
-                            this.state.methods && this.state.methods.data &&
-                            <BarChart config={this.state.methods}/>
-                        }
-                        {this.state.errorG1 && <MensajeErrorDatos/>}
-                    </Grid>
-                    <Grid item xs={12} md={4} className={classes.descripcion}>
-                        <Typography variant={"body1"}>
-                            Las resoluciones son : Absolutoria, Sancionatoria, Sancionatoria con multa, Sancionatoria
-                            con multa e inhabilitación, y se agrega la categoría "No específica" por aquellos registros
-                            que no incluyen dicho valor.
-                            Como muestra la gráfica de la izquierda, del 2004 a mayo 2021, de las <b>2,074
-                            resoluciones</b> la mayoría finalizó en <b>sanción con multa e inhabilitación</b> con un
-                            total de <b>1,516</b> en total, seguido por <b>462</b> sanciones con
-                            multa, <b>9</b> sancionatorias, <b>2</b> absolutorias y <b>24</b> no especifican el sentido
-                            de la resolución.
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} className={classes.descripcion}>
-                        <Typography variant={"body1"}>
-                            A lo largo del tiempo, se aprecia que cada año el mayor número de sanciones a particulares
-                            son <b>Sancionatorias con multa e inhabilitación</b>.
-                            Seguido de la resolución <b>Sancionatoria con multa</b> que cuenta con registros a
-                            partir del año 2008 hasta el 2021
-                            Posteriormente se tiene la resolución <b>Sancionatoria</b>, la cual aparece en el año
-                            2009, 2015 y del 2019 a 2021.
-                            Por otra parte, se tienen dos registros de resolución <b>Absolutoria</b>, las cuales
-                            fueron resueltas en 2019.
-                            <br/><br/>Cabe señalar que las 24 sanciones que no especifican el tipo de resolución, tampoco
-                            cuentan con información respecto a su fecha de resolución, por lo que, no se encuentran
-                            graficadas.
+    return (
+        <div>
+            <Grid container spacing={0} justifyContent='center' className={classes.frameChart}>
+                <Grid item xs={12}>
+                    <Typography variant={"h6"} className={classes.titulo}>
+                        <b> Sentido de las resoluciones</b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={8} className={classes.graph}>
+                    {
+                        methods && methods.data &&
+                        <BarChart config={methods}/>
+                    }
+                    {errorG1 && <MensajeErrorDatos/>}
+                </Grid>
+                <Grid item xs={12} md={4} className={classes.descripcion}>
+                    <Typography variant={"body1"}>
+                        Las resoluciones son : Absolutoria, Sancionatoria, Sancionatoria con multa, Sancionatoria
+                        con multa e inhabilitación, y se agrega la categoría "No específica" por aquellos registros
+                        que no incluyen dicho valor.
+                        Como muestra la gráfica de la izquierda, del 2004 a mayo 2021, de las <b>2,074
+                        resoluciones</b> la mayoría finalizó en <b>sanción con multa e inhabilitación</b> con un
+                        total de <b>1,516</b> en total, seguido por <b>462</b> sanciones con
+                        multa, <b>9</b> sancionatorias, <b>2</b> absolutorias y <b>24</b> no especifican el sentido
+                        de la resolución.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} className={classes.descripcion}>
+                    <Typography variant={"body1"}>
+                        A lo largo del tiempo, se aprecia que cada año el mayor número de sanciones a particulares
+                        son <b>Sancionatorias con multa e inhabilitación</b>.
+                        Seguido de la resolución <b>Sancionatoria con multa</b> que cuenta con registros a
+                        partir del año 2008 hasta el 2021
+                        Posteriormente se tiene la resolución <b>Sancionatoria</b>, la cual aparece en el año
+                        2009, 2015 y del 2019 a 2021.
+                        Por otra parte, se tienen dos registros de resolución <b>Absolutoria</b>, las cuales
+                        fueron resueltas en 2019.
+                        <br/><br/>Cabe señalar que las 24 sanciones que no especifican el tipo de resolución, tampoco
+                        cuentan con información respecto a su fecha de resolución, por lo que, no se encuentran
+                        graficadas.
 
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                        {
-                            this.state.config2 && this.state.config2.data &&
-                            <BarChart config={this.state.config2}/>
-                        }
-                        {this.state.errorG2 && <MensajeErrorDatos/>}
-                    </Grid>
-
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    {
+                        config2 && config2.data &&
+                        <BarChart config={config2}/>
+                    }
+                    {errorG2 && <MensajeErrorDatos/>}
                 </Grid>
 
-            </div>
-        )
-    }
+            </Grid>
+        </div>
+    )
+
 
 }
 
