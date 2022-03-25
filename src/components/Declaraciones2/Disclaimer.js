62626679efe74bcef0d7d3baf2fc4bdade50dc4f
Benito Perdomo <benito.perdomo.v@gmail.com>
2022-03-24 18:05:55 -0600

    FIX console log p over p

--- src/components/Declaraciones2/Disclaimer.js
+++ src/components/Declaraciones2/Disclaimer.js
@@ -13,32 +13,26 @@ export default function Disclaimer(props) {
   return (
     <Dialog open={props.open} onClose={props.handleClose} aria-labelledby='alert-dialog-title' aria-describedby='alert-dialog-description'>
       <Paper className={classes.paper_search} style={{ margin: 0, borderRadius: 0 }}>
-        <DialogTitle id='alert-dialog-title'>
-          <Typography paragraph className={classes.text_color}>
-            Plataforma Digital Nacional
-          </Typography>
+        <DialogTitle id='alert-dialog-title' className={classes.text_color}>
+          Plataforma Digital Nacional
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id='alert-dialog-description' style={{ textAlign: 'justify' }}>
-            <Typography paragraph className={classes.text_color}>
-              La totalidad de la información que se muestra en este Sistema es responsabilidad exclusiva de quien la genera, concentra y provee a esta Plataforma. Esto está establecido en las{' '}
-              <Link href='https://www.dof.gob.mx/nota_detalle.php?codigo=5541802&fecha=23/10/2018' target='_blank' underline='none' className={classes.enlaces}>
-                Bases para el Funcionamiento de la Plataforma Digital Nacional
-              </Link>{' '}
-              (Artículos: 3, 6, 7 y 8 ) y la{' '}
-              <Link href='https://www.diputados.gob.mx/LeyesBiblio/pdf/LGRA.pdf' target='_blank' underline='none' className={classes.enlaces}>
-                Ley General de Responsabilidades Administrativas
-              </Link>{' '}
-              (Artículos: 31 y 32).
-            </Typography>
+          <DialogContentText paragraph id='alert-dialog-description' style={{ textAlign: 'justify' }} className={classes.text_color}>
+            La totalidad de la información que se muestra en este Sistema es responsabilidad exclusiva de quien la genera, concentra y provee a esta Plataforma. Esto está establecido en las{' '}
+            <Link href='https://www.dof.gob.mx/nota_detalle.php?codigo=5541802&fecha=23/10/2018' target='_blank' underline='none' className={classes.enlaces}>
+              Bases para el Funcionamiento de la Plataforma Digital Nacional
+            </Link>{' '}
+            (Artículos: 3, 6, 7 y 8 ) y la{' '}
+            <Link href='https://www.diputados.gob.mx/LeyesBiblio/pdf/LGRA.pdf' target='_blank' underline='none' className={classes.enlaces}>
+              Ley General de Responsabilidades Administrativas
+            </Link>{' '}
+            (Artículos: 31 y 32).
           </DialogContentText>
-          <DialogContentText id='alert-dialog-description2' style={{ textAlign: 'justify' }}>
-            <Typography paragraph className={classes.text_color}>
-              Te invitamos a revisar los{' '}
-              <RouterLink to={'/terminos'} className={classes.enlaces}>
-                "Términos y Condiciones de Uso"
-              </RouterLink>
-            </Typography>
+          <DialogContentText paragraph id='alert-dialog-description2' style={{ textAlign: 'justify' }} className={classes.text_color}>
+            Te invitamos a revisar los{' '}
+            <RouterLink to={'/terminos'} className={classes.enlaces}>
+              "Términos y Condiciones de Uso"
+            </RouterLink>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
