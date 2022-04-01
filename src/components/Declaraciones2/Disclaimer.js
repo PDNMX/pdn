d474f05df0972968155fee4565c21a3de6e772f3
Benito Perdomo <benito.perdomo.v@gmail.com>
2022-04-01 10:32:34 -0600

    UPDATE disclaimer texto

--- src/components/Declaraciones2/Disclaimer.js
+++ src/components/Declaraciones2/Disclaimer.js
@@ -18,20 +18,22 @@ export default function Disclaimer(props) {
         </DialogTitle>
         <DialogContent>
           <DialogContentText paragraph id='alert-dialog-description' style={{ textAlign: 'justify' }} className={classes.text_color}>
-            La totalidad de la información que se muestra en este Sistema es responsabilidad exclusiva de quien la genera, concentra y provee a esta Plataforma. Esto está establecido en las{' '}
+            Toda la información que se muestra en este Sistema es responsabilidad de quien la genera, concentra y provee a la PDN.
+          </DialogContentText>
+          <DialogContentText paragraph id='alert-dialog-description' style={{ textAlign: 'justify' }} className={classes.text_color}>
+            Lo anterior se encuentra fundamentado en las{' '}
             <Link href='https://www.dof.gob.mx/nota_detalle.php?codigo=5541802&fecha=23/10/2018' target='_blank' underline='none' className={classes.enlaces}>
               Bases para el Funcionamiento de la Plataforma Digital Nacional
             </Link>{' '}
-            (Artículos: 3, 6, 7 y 8 ) y la{' '}
+            (artículos 3, 6, 7 y 8) y en la{' '}
             <Link href='https://www.diputados.gob.mx/LeyesBiblio/pdf/LGRA.pdf' target='_blank' underline='none' className={classes.enlaces}>
               Ley General de Responsabilidades Administrativas
             </Link>{' '}
-            (Artículos: 31 y 32).
+            (artículos 31 y 32).
           </DialogContentText>
-          <DialogContentText paragraph id='alert-dialog-description2' style={{ textAlign: 'justify' }} className={classes.text_color}>
-            Te invitamos a revisar los{' '}
+          <DialogContentText paragraph id='alert-dialog-description' style={{ textAlign: 'justify' }} className={classes.text_color}>
             <RouterLink to={'/terminos'} className={classes.enlaces}>
-              "Términos y Condiciones de Uso"
+              Conoce los términos y condiciones de uso
             </RouterLink>
           </DialogContentText>
         </DialogContent>
