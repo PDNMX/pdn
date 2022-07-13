06dd8f71239df58c091a5b539db2ac779c650d15
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2022-07-12 23:43:51 -0500

    busquedas

--- src/components/HomeV2/Buscador/ResultadosBusqueda/EmpresasTienenContratosGob.js
+++ src/components/HomeV2/Buscador/ResultadosBusqueda/EmpresasTienenContratosGob.js
@@ -1,13 +1,248 @@
 import React from "react";
-//import { InputLabel, FormControl, TextField, Select, MenuItem } from "@mui/material/";
-//import { useFormContext, } from "react-hook-form";
+import TablaResultados from "../../../Sistema6/TablaResultados";
+import axios from "axios";
+
+/* const styles = theme => ({
+    root: {
+        flexGrow: 1
+    },
+}); */
 
 export function ResultadosEmpresasTienenContratosGob(props) {
-    //const { control } = useFormContext();
-    let data = props.data;
-    return (
-      <>
-        <p>{data}</p><br/>
-      </>
-    );
+  let data = props.data;
+  const [state, setState] = React.useState({
+    dataSupplier: "SHCP",
+    inputText: "",
+    pagination: {
+      pageSize: 10,
+      page: 0,
+      total: 0,
+    },
+    results: [],
+    loading: false,
+    buyers: [],
+    buyer_id: "any",
+    procurementMethod: "any",
+    supplierName: "",
+    cycle: "any",
+    cycles: [],
+  });
+
+  React.useEffect(() => {
+    //fetch data
+    const supplier_id = props.dataSupplier; //state.dataSupplier;
+
+    const _buyers = () =>
+      axios({
+        url: process.env.REACT_APP_S6_BACKEND + "/api/v1/buyers",
+        params: {
+          supplier_id,
+        },
+        method: "GET",
+        json: true,
+      });
+
+    const _search = () =>
+      axios({
+        url: process.env.REACT_APP_S6_BACKEND + "/api/v1/search",
+        params: {
+          supplier_id,
+        },
+        method: "POST",
+        json: true,
+      });
+
+    const _cycles = () =>
+      axios({
+        url: process.env.REACT_APP_S6_BACKEND + "/api/v1/cycles",
+        params: {
+          supplier_id,
+        },
+        method: "GET",
+        json: true,
+      });
+
+    Promise.all([_buyers(), _search(), _cycles()])
+      .then((res) => {
+        //console.log (res);
+        setState({
+          ...state,
+          dataSupplier: supplier_id, //
+          buyers: res[0].data,
+          pagination: res[1].data.pagination,
+          results: res[1].data.data,
+          cycles: res[2].data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    if (state.loading) {
+      search();
+    }
+  }, [
+    state.loading,
+    /*
+        state.pagination.pageSize,
+        state.pagination.page,
+        state.buyer_id,
+        state.procurementMethod,
+        state.cycle
+        state.inputText, // la búsqueda se ejecuta hasta dar click en el boton 'BUSCAR'
+        state.supplierName // la búsqueda se ejecuta hasta dar click en el boton 'BUSCAR'
+        */
+  ]);
+
+  React.useEffect(() => {
+    /**
+     * Cambiar dataSupplier
+     * obtener las listas de buyers y cycles
+     * limpiar state, asignar dataSupplier y ejecutar búsqueda inicial
+     */
+
+    if (props.dataSupplier && props.dataSupplier !== state.dataSupplier) {
+      console.log(`Setting data supplier to => ${props.dataSupplier}`);
+      handleChangeDS(props.dataSupplier);
+    }
+  }, [props.dataSupplier]);
+
+  const handleChangeDS = async (dataSupplier) => {
+    const _buyers = await axios({
+      url: process.env.REACT_APP_S6_BACKEND + "/api/v1/buyers",
+      params: {
+        supplier_id: dataSupplier,
+      },
+      method: "GET",
+      json: true,
+    });
+
+    const _cycles = await axios({
+      url: process.env.REACT_APP_S6_BACKEND + "/api/v1/cycles",
+      params: {
+        supplier_id: dataSupplier,
+      },
+      method: "GET",
+      json: true,
+    });
+
+    setState({
+      ...state,
+      dataSupplier: dataSupplier,
+      buyers: _buyers.data,
+      cycles: _cycles.data,
+      pagination: {
+        pageSize: 10,
+        page: 0,
+        total: 0,
+      },
+      buyer_id: "any",
+      procurementMethod: "any",
+      supplierName: "",
+      cycle: "any",
+      loading: true,
+    });
   };
+
+  const handleChangeRowsPerPage = (pageSize) => {
+    setState({
+      ...state,
+      loading: true,
+      pagination: {
+        page: 0,
+        pageSize: pageSize,
+        //total: 0
+      },
+    });
+  };
+
+  const handlePageChange = (page) => {
+    setState({
+      ...state,
+      loading: true,
+      pagination: {
+        page: page, //incrementar página
+        pageSize: state.pagination.pageSize,
+        //total: 0
+      },
+    });
+  };
+
+  //buscar
+  const search = async () => {
+    let body = {
+      page: state.pagination.page,
+      pageSize: state.pagination.pageSize,
+    };
+
+    if (state.buyer_id !== "any") {
+      body.buyer_id = state.buyer_id;
+    }
+
+    if (state.procurementMethod !== "any") {
+      body.procurementMethod = state.procurementMethod;
+    }
+
+    if (state.supplierName !== "") {
+      body.supplierName = state.supplierName;
+    }
+
+    if (state.inputText !== "") {
+      body.tender_title = state.inputText;
+    }
+
+    if (state.cycle !== "any") {
+      body.cycle = state.cycle;
+    }
+
+    const supplier_id = state.dataSupplier;
+
+    //console.log(body);
+    try {
+      const res = await axios({
+        url: process.env.REACT_APP_S6_BACKEND + "/api/v1/search",
+        params: {
+          supplier_id,
+        },
+        method: "POST",
+        data: body,
+        json: true,
+      });
+
+      //console.log(data)
+      setState({
+        ...state,
+        loading: false,
+        results: res.data.data,
+        pagination: res.data.pagination, //solo debe actualizarse el total
+      });
+    } catch (error) {
+      console.log(error);
+      setState({
+        ...state,
+        loading: false,
+      });
+    }
+  };
+
+  //const {classes} = props;
+
+  return (
+    <div>
+      <div style={{ overflow: "auto" }}>
+        <TablaResultados
+          data={state.results}
+          pagination={state.pagination}
+          handleChangeRowsPerPage={handleChangeRowsPerPage}
+          handleChangePage={handlePageChange}
+          loading={state.loading}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ResultadosEmpresasTienenContratosGob;
--- src/components/HomeV2/Buscador/ResultadosBusqueda/PersonasServidorasPublicasSancionados.js
+++ src/components/HomeV2/Buscador/ResultadosBusqueda/PersonasServidorasPublicasSancionados.js
@@ -1,14 +1,376 @@
 import React from "react";
-//import { InputLabel, FormControl, TextField, Select, MenuItem } from "@mui/material/";
-//import { useFormContext, } from "react-hook-form";
+import withStyles from "@mui/styles/withStyles";
+import { Grid, Modal, CircularProgress } from "@mui/material";
+import MensajeErrorDatos from "../../../Mensajes/MensajeError";
+import Previos from "../../../Compartidos/Previos";
+import TablaServidoresSancionados from "../../../Sancionados/Servidores/TablaServidoresSancionados";
+import DetalleServidorSancionado from "../../../Sancionados/Servidores//DetalleServidorSancionado";
+import { ThemeProvider } from "@mui/material/styles";
+import ThemeV2 from "../../../../ThemeV2";
+
+//import ReactGA from "react-ga";
+
+const axios = require("axios");
+
+const styles = (theme) => ({
+  formControl: {
+    width: "100%",
+  },
+  "&$focus": {
+    //color: theme.palette.black.color,
+  },
+  centrado: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  progress: {
+    position: "fixed",
+    margin: "auto",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  desc: {
+    color: theme.palette.text.contrastText,
+  },
+  container: {
+    /* marginTop: '30px',
+        marginBottom: '30px',*/
+    overflowX: "auto",
+  },
+  section: {
+    maxWidth: "1200px",
+    overflowX: "auto",
+    padding: theme.spacing(1),
+  },
+  button: {
+    padding: theme.spacing(1),
+    fontWeight: "bold",
+  },
+});
+
+const tiposSancion = [
+  { label: "Inhabilitado", value: "I" },
+  { label: "Multado", value: "M" },
+  { label: "Suspensión del empleo, cargo o comisión", value: "S" },
+  { label: "Destitución del empleo, cargo o comisión", value: "D" },
+  { label: "Indemnización resarcitoria", value: "IRSC" },
+  { label: "Sanción económica", value: "SE" },
+  { label: "Otro", value: "O" },
+];
+
+const camposOrdenamiento = [
+  { label: "Nombre", value: "nombres" },
+  { label: "Apellido Uno", value: "primerApellido" },
+  { label: "Apellido Dos", value: "segundoApellido" },
+  { label: "Institución", value: "institucionDependencia" },
+];
+
+const tiposOrdenamiento = [
+  { label: "Ascendente", value: "asc" },
+  { label: "Descendente", value: "desc" },
+];
+
+const initialPagination = {
+  page: 1,
+  rowsPerPage: 10,
+  totalRows: 0,
+};
+
+const initialFilter = {
+  nombres: "",
+  primerApellido: "",
+  segundoApellido: "",
+  tipoSancion: [],
+  institucionDependencia: "any",
+  nivel: "any",
+  provider: "any",
+};
+
+const initialSort = {
+  campoOrden: "any",
+  tipoOrden: "any",
+};
 
 export function ResultadosPersonasServidorasPublicasSancionados(props) {
-    //const { control } = useFormContext();
-    let data = JSON.parse(props.data);
-    //console.log(data);
-    return (
-      <>
-        <p>{data.tipoBusqueda}</p><br/>
-      </>
-    );
+  const data = JSON.parse(props.data);
+  
+  const [filterData, setFilterData] = React.useState([]);
+  const [previos, setPrevios] = React.useState(null);
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [institutionsList, setInstitutionsList] = React.useState([]);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+  const [provider, setProvider] = React.useState("any");
+  const [providersList, setProvidersList] = React.useState([]);
+  const [pagination, setPagination] = React.useState(initialPagination);
+  const [filter, setFilter] = React.useState(initialFilter);
+  const [sort, setSort] = React.useState(initialSort);
+  const [view, setView] = React.useState(0);
+
+  React.useEffect(() => {
+    //loadInstitutions();
+    //loadProviders();
+    handleSearchPrevios();
+    
+  }, []);
+
+  React.useEffect(() => {
+    loadInstitutions();
+    loadProviders();
+  }, [filter?.nivel]);
+
+  React.useEffect(() => {
+    loadInstitutions();
+  }, [filter?.provider]);
+
+  React.useEffect(() => {
+    if (provider !== "any") {
+      setPagination({ ...pagination, page: 1 });
+      setSelectedItem(null);
+      handleSearchAPI();
+    }
+  }, [provider]);
+
+  const loadInstitutions = () => {
+    let sug = [];
+    let options = {
+      url: process.env.REACT_APP_S3S_BACKEND + "/api/v1/entities",
+      json: true,
+      method: "post",
+      data: {},
+    };
+    if (filter.nivel !== "any") options.data.nivel_gobierno = filter.nivel;
+    if (filter.provider !== "any")
+      options.data.supplier_id = filter.provider.key;
+
+    axios(options)
+      .then((data) => {
+        data.data.forEach((item, index) => {
+          sug.push({ value: item.nombre, label: item.nombre, key: index });
+        });
+        setInstitutionsList(sug);
+        setFilter({ ...filter, institucionDependencia: "any" });
+      })
+      .catch((err) => {
+        setError(true);
+      });
   };
+
+  const loadProviders = () => {
+    let sug = [];
+    let options = {
+      url: process.env.REACT_APP_S3S_BACKEND + "/api/v1/getProviders",
+      json: true,
+      method: "post",
+      data: {},
+    };
+    if (filter.nivel !== "any") options.data.nivel_gobierno = filter.nivel;
+    axios(options)
+      .then((data) => {
+        data.data.forEach((provider) => {
+          sug.push({
+            value: provider.supplier_id,
+            label: provider.supplier_name,
+            key: provider.supplier_id,
+          });
+        });
+        setProvidersList(sug);
+        setProvider("any");
+      })
+      .catch((err) => {
+        setError(true);
+      });
+  };
+
+  const handleCleanAll = () => {
+    setFilter(initialFilter);
+    setProvidersList([]);
+    setPagination(initialPagination);
+    setSort(initialSort);
+    setFilterData(null);
+    setPrevios([]);
+    setInstitutionsList([]);
+    setSelectedItem(null);
+    setView(0);
+  };
+  //10,25,50
+  const handleSearchPrevios = () => {
+    //ReactGA.event({category: 'busqueda-s3SP', action: 'click'});
+    setLoading(true);
+    setFilterData([]);
+    setSelectedItem(null);
+    setPagination({ ...pagination, rowsPerPage: 10 });
+    setProvider("any");
+    
+    let body = {
+      query: makeFiltros(),
+      institucion: filter.institucionDependencia,
+    };
+
+    if (filter.nivel !== "any") body.nivel_gobierno = filter.nivel;
+    if (filter.provider !== "any") body.proveedor = filter.provider.key;
+    let options = {
+      method: "POST",
+      url: process.env.REACT_APP_S3S_BACKEND + "/api/v1/summary",
+      json: true,
+      data: body,
+    };
+    axios(options)
+      .then((res) => {
+        setPrevios(res.data);
+        setLoading(false);
+        setError(false);
+        setView(1);
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
+      });
+  };
+
+  const makeFiltros = () => {
+    let filtros = {};
+    if (filter.nombres) filtros.nombres = filter.nombres;
+    if (filter.primerApellido) filtros.primerApellido = filter.primerApellido;
+    if (filter.segundoApellido)
+      filtros.segundoApellido = filter.segundoApellido;
+    if (
+      filter.institucionDependencia &&
+      filter.institucionDependencia !== "any"
+    )
+      filtros.institucionDependencia = filter.institucionDependencia;
+    if (filter.tipoSancion.length > 0)
+      filtros.tipoSancion = filter.tipoSancion.map((item) => item.value);
+    return filtros;
+  };
+
+  const makeSort = () => {
+    let sort_ = {};
+    if (sort.campoOrden !== "any" && sort.tipoOrden !== "any")
+      sort_[sort.campoOrden.value] = sort.tipoOrden.value;
+    return sort_;
+  };
+
+  const handleSearchAPI = () => {
+    setLoading(true);
+    let body = {
+      query: makeFiltros(),
+      pageSize: pagination.rowsPerPage,
+      page: pagination.page,
+      supplier_id: provider,
+      sort: makeSort(),
+    };
+
+    let options = {
+      method: "POST",
+      url: process.env.REACT_APP_S3S_BACKEND + "/api/v1/search",
+      json: true,
+      data: body,
+    };
+
+    axios(options)
+      .then((res) => {
+        let resultado = res.data;
+        setFilterData(resultado.results);
+        setLoading(false);
+        setPagination({
+          ...pagination,
+          totalRows: resultado.pagination.totalRows,
+        });
+        setError(false);
+        setView(2);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(true);
+      });
+  };
+
+  const handleChangeAPI = (val) => {
+    setProvider(val);
+  };
+
+  const handleChangePage = (event, page) => {
+    setPagination({ ...pagination, page: page + 1 });
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setPagination({ ...pagination, rowsPerPage: event.target.value, page: 1 });
+  };
+
+  const verDetalle = (event, elemento) => {
+    setSelectedItem(elemento);
+    setView(3);
+    setProvider("any");
+  };
+
+  const handleChangeDetail = () => {
+    setSelectedItem(null);
+    setView(2);
+  };
+
+  const returnToPrevios = () => {
+    setView(1);
+    setProvider("any");
+  };
+
+  return (
+      <React.Fragment>
+        {/*Buscador*/}
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            {loading && (
+              <Modal open={loading} disableAutoFocus={true}>
+                <CircularProgress id="spinnerLoading" size={200} />
+              </Modal>
+            )}
+            {error && <MensajeErrorDatos />}
+          </Grid>
+        </Grid>
+        {/*PREVIOS*/}
+        {view === 1 && previos && previos.length > 0 && (
+          <Grid container>
+            <Grid item xs={12}>
+              <Previos
+                data={previos}
+                handleChangeSujetoObligado={handleChangeAPI}
+              />
+            </Grid>
+          </Grid>
+        )}
+        {/*TABLA*/}
+        {view === 2 &&
+          filterData &&
+          filterData.length > 0 &&
+          selectedItem === null && (
+            <Grid container>
+              <Grid item xs={12}>
+                <TablaServidoresSancionados
+                  data={filterData}
+                  page={pagination.page}
+                  rowsPerPage={pagination.rowsPerPage}
+                  totalRows={pagination.totalRows}
+                  handleChangePage={handleChangePage}
+                  handleChangeRowsPerPage={handleChangeRowsPerPage}
+                  verDetalle={verDetalle}
+                  returnToPrevios={returnToPrevios}
+                />
+              </Grid>
+            </Grid>
+          )}
+        {view === 3 && selectedItem !== null && (
+          <DetalleServidorSancionado
+            handleChangeDetail={handleChangeDetail}
+            servidor={selectedItem}
+          />
+        )}
+      </React.Fragment>
+  );
+}
+
+export default withStyles(styles, { withTheme: false })(
+  ResultadosPersonasServidorasPublicasSancionados
+);
