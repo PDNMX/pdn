263855648a16a8acca278a8fe29986b43f505517
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2022-08-04 18:59:12 -0500

    altura cards

--- src/components/HomeV2/Buscador/LinearStepper.js
+++ src/components/HomeV2/Buscador/LinearStepper.js
@@ -64,22 +64,18 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   cardMedia: {
-    height: "100%",
     width: "30%",
     margin: "3% 0%",
-    objectFit: "cover",
+
   },
   card: {
-    /* background: "#fff", */
-    /* width: "24em", */
+    background: "#E1E8EB",
     borderRadius: "0.6em",
     margin: "2%",
-    overflow: "hidden",
-    /* cursor: "pointer", */
     boxShadow:
       "0 13px 27px -5px hsla(240, 30.1%, 28%, 0.25), 0 8px 16px -8px hsla(0, 0%, 0%, 0.3), 0 -6px 16px -6px hsla(0, 0%, 0%, 0.03)",
     transition: "all ease 200ms",
-    height: "100%",
+    
     "&:hover": {
       transform: "scale(1.03)",
       boxShadow:
@@ -272,13 +268,13 @@ const LinaerStepper = ({ stateChanger, ...rest }) => {
               container
               spacing={0}
               direction="row"
-              alignItems="center"
+              alignItems="stretch"
               justifyContent="center"
               className={classes.container}
             >
               {opciones.map((opcion, index) => (
-                <Grid key={index} item md={4}>
-                  <Card className={classes.card} {...field}>
+                <Grid key={index} item md={4} style={{display: 'flex'}}>
+                  <Card className={classes.card} {...field} style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column'}}>
                     <Tooltip
                       title={opcion.detalle}
                       TransitionComponent={Zoom}
@@ -298,7 +294,6 @@ const LinaerStepper = ({ stateChanger, ...rest }) => {
                           className={classes.cardMedia}
                           component="img"
                           image={"./asistente_busqueda/" + opcion.img}
-                          alt="ejemplo"
                         />
                         <CardContent>
                           <Typography gutterBottom variant="subtitle1">
