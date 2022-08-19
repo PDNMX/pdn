3cd1bed16d870fb71a9c62075196a07b2674cb28
Mario Torres <mario291187@gmail.com>
2022-08-19 15:08:54 -0500

    Cobertura: estados

--- src/components/Cobertura/Cobertura.js
+++ src/components/Cobertura/Cobertura.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import {Typography, Link, Grid} from "@mui/material";
+import {Typography,Box, Link, Grid, Paper, Tabs, Tab} from "@mui/material";
 import {Link as RouterLink, useParams} from 'react-router-dom';
 import HeaderV2 from "../HomeV2/HeaderV2";
 import pdnRoutes from "../../routes";
 import estados from "./estados.json";
 
-const Cobertura = () => {
+import withStyles from "@mui/styles/withStyles";
+const styles = theme => ({
+    estado: {
+        width:"150px", borderStyle: 'solid', border: '1px', borderRadius: 10, color: '#d3d3d3',
+        background: theme.palette.background.opaque
+    },
+    link:{
+        textDecoration: 'none'
+    }
+});
+
+const Cobertura = props => {
     const section = pdnRoutes.find(r => r.path === '/cobertura');
+    const {classes} = props;
     return (<div>
             <HeaderV2 section={section}/>
             <Grid container spacing={0} justifyContent="center">
                 <Grid item xs={12} style={{maxWidth: 1200}}>
-                    <ul>
+
+                    {/*<Paper elevation={15}>*/}
+                    <Box display="flex" flexWrap="wrap">
                         {estados.map( e =>{
-                            return <li><Link component={RouterLink} to={e.route}>{e.name}</Link></li>
+                            const icon = require(`../../assets/Cobertura/iconos_estados/${e.icon}`)
+                            return <Box textAlign="center" p={1} m={1} className={classes.estado}>
+                                <Link component={RouterLink} to={e.route} className={classes.link}>
+                                    <img src={icon} style={{width: 100}}/>
+                                    <Typography>{e.name}</Typography>
+                                </Link>
+                            </Box>
                         })
                         }
-                    </ul>
+                    </Box>
+                    {/*</Paper>*/}
+
                 </Grid>
             </Grid>
         </div>
     );
 }
 
-export default Cobertura;
+export default withStyles(styles)(Cobertura);
