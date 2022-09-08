3c48eb796fbf8efdcb3be56ec5717f29275bc1ea
Mario Torres <mario291187@gmail.com>
2022-09-08 15:15:20 -0500

    Acceso herramienta evolucion

--- src/components/HomeV2/Evolucion.js
+++ src/components/HomeV2/Evolucion.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import {withStyles} from "@mui/styles";
 import {Box, Button, Typography} from '@mui/material';
-
-import logo from '../../assets/ad/logo_semanaPDN.svg';
-import bgimg from '../../assets/ad/fondo_banner_SemanaPDN.png';
-import manitad from '../../assets/ad/backhand-index-pointing-right-joypixels.gif';
-import manitai from '../../assets/ad/backhand-index-pointing-left-joypixels.gif';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import logo from '../../assets/ico-evolucion.svg';
 
 const styles = theme => ({
     root: {
         flexGrow: 1,
-        height: '230px',
-        backgroundColor: '#f5f5f5',
-        backgroundImage: `url(${bgimg})`,
+        minHeight: '200px',
+        backgroundColor: theme.palette.background.paperChart,
         backgroundSize: 'cover',
-        backgroundPosition: 'center'
+        backgroundPosition: 'center',
+        padding: theme.spacing(2)
     },
-    logo:{
-        paddingTop: theme.spacing(2),
-        maxWidth: '300px'
+    logo: {
+        width: '130px'
     },
     btnPDN: {
-        'margin': theme.spacing(1),
-        'background': 'white',//'#9eb1b6',
-        'borderRadius': '50px',
-        'fontWeight': 'bold',
-        'fontStyle': 'italic',
+        margin: theme.spacing(1),
+        background: "rgb(255,255,255,0.5)",
+        borderRadius: "50px",
+        fontWeight: "bold",
+        fontStyle: "italic",
         '&:hover': {
-            backgroundColor: '#56a3bf'
-        }
+            backgroundColor: "#56a3bf",
+        },
     },
     manitad:{
         paddingRight: '6px',
@@ -36,30 +32,38 @@ const styles = theme => ({
     },
     manitai:{
         maxHeight: '40px'
+    },
+    headingText: {
+        color: theme.palette.text.main,
+        fontWeight: "bold",
+        maxWidth: '450px'
     }
 });
 
 const Evolucion = props => {
     const {classes} = props;
-    const url = "https://dev.plataformadigitalnacional.org/evolucion";
+    const url = "https://evolucion.plataformadigitalnacional.org";
 
     return <div className={classes.root}>
-        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-            <img src={logo} alt='Semana PDN' className={classes.logo}/>
-        </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-            <Typography variant="h5">
-                Verificación de evolucion patrimonial y de intereses
-            </Typography>
-        </Box>
+        <Box sx={{ display: 'flex', flexWrap: "wrap", justifyContent: 'center'}}>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-            <img src={manitad} className={classes.manitad} alt='Manita' />
-            <Button className={classes.btnPDN} href={url} target='_blank' variant='contained'>
-                Acceso
-            </Button>
-            <img src={manitai} className={classes.manitai} alt='Manita' />
+            <Box p={3}>
+                <img src={logo} alt='Semana PDN' className={classes.logo}/>
+            </Box>
+
+            <Box p={3}>
+                <Typography variant="h5" className={classes.headingText} paragraph>
+                    Herramienta de Verificación de
+                    Evolucion patrimonial y de intereses
+                </Typography>
+
+                <Button className={classes.btnPDN} href={url} target='_blank' variant='contained'
+                        startIcon={<DirectionsWalkIcon/>}>
+                    Acceso
+                </Button>
+            </Box>
+
         </Box>
     </div>
 }
