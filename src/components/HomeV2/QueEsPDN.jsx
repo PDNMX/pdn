8d08ff0d1f642f9941c6ae000a184226d575db6d
RBalan21 <aln.rojas.pdn@gmail.com>
2024-01-11 16:37:06 -0600

    fix dialogo principal

--- src/components/HomeV2/QueEsPDN.jsx
+++ src/components/HomeV2/QueEsPDN.jsx
@@ -53,7 +53,7 @@ const QueEsPDN = props => {
                     <BuscadorModal />
                 </Grid>
                 <Grid item md={6} sm={12} xs={12} align="left" >
-                    <Typography >
+                    <Typography marginTop={'20px'} >
                     La <b>Plataforma Digital Nacional (PDN)</b> del Sistema Nacional Anticorrupción (SNA) es un <b>instrumento de inteligencia</b> que tiene como objetivo eliminar las barreras de información para que los datos públicos sean comparables, accesibles y utilizables a efecto de combatir cualquier acto de corrupción.<br/><br/>
                     <b>La Secretaría Ejecutiva del Sistema Nacional Anticorrupción (SESNA)</b>, organismo descentralizado no sectorizado, es responsable de administrar la <b>PDN</b>.<br/><br/>
                     La PDN no es generadora ni un repositorio de datos, sino una plataforma de <b>interoperabilidad</b> que consulta información de diversas fuentes.<br/><br/>
