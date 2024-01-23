c9c5b7594fe1db55c39707a05a1db3aea2b67c31
Benito Perdomo <benito.perdomo.v@gmail.com>
2024-01-23 11:14:45 -0600

    add new process

---
+++ src/__test__/utils/readFiles/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const jsonfile = require('jsonfile');
+const fs = require('fs');
+
+let _dir = `${__dirname}/data/`;
+
+const readFiles = () => {
+  let info = [];
+
+  try {
+    const data = fs.readdirSync(_dir, { withFileTypes: true });
+    data.map(file => {
+      let data = [];
+      if (file.name.includes('.json')) {
+        const _file = `${_dir}/${file.name}`;
+        const content = jsonfile.readFileSync(_file, { encoding: 'utf8', flag: 'r' });
+
+        content.forEach(d => {
+          data.push(d);
+        });
+
+        info.push({ name: file.name, data });
+      }
+    });
+  } catch (error) {
+    console.log({ error });
+  }
+
+  return info;
+};
+
+module.exports = {
+  readFiles
+};
