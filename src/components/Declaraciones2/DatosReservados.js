9f9ed1714b03536274a4b4c09e752d2393a0dc0a
Benito Perdomo <benito.perdomo.v@gmail.com>
2024-02-02 12:47:12 -0600

    update message datos reservado a no publicos

--- src/components/Declaraciones2/DatosReservados.js
+++ src/components/Declaraciones2/DatosReservados.js
@@ -7,15 +7,15 @@ import style from './styleSecciones';
 const useStyles = makeStyles(style);
 
 export default function DatosReservados() {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<Grid item xs={12}>
-			<Paper className={classes.paper}>
-				<Typography className={classes.alertInfo} align="center">
-					Los datos contenidos en esta seccion son de carácter reservado.
-				</Typography>
-			</Paper>
-		</Grid>
-	);
+  return (
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>
+        <Typography className={classes.alertInfo} align='center'>
+          Los datos contenidos en esta sección no son públicos
+        </Typography>
+      </Paper>
+    </Grid>
+  );
 }
