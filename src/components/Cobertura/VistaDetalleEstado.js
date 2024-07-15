a12e57968a1db1e2e1fb9006e45e14105577ea1d
RBalan21 <aln.rojas.pdn@gmail.com>
2024-07-15 16:19:05 -0600

    update cobertura 2do trimestre 2024

--- src/components/Cobertura/VistaDetalleEstado.js
+++ src/components/Cobertura/VistaDetalleEstado.js
@@ -151,7 +151,7 @@ const VistaDetalleEstado = props => {
                     </Typography>
 
                     <Typography align="center" color="white" paragraph>
-                        Información al 31 de marzo de 2024, reportada por la Secretaría Ejecutiva del Sistema Estatal Anticorrupción 
+                        Información al 30 de junio de 2024, reportada por la Secretaría Ejecutiva del Sistema Estatal Anticorrupción 
                     </Typography>
 
                     <Box sx={{display: 'flex', flexWrap: "wrap", alignItems: "stretch"}} justifyContent="center">
