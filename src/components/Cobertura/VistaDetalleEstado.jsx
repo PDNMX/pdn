800f23a8d17c42bff6e4b2e1d2f791d1fb57dffd
RBalan21 <aln.rojas.pdn@gmail.com>
2024-12-11 16:53:43 -0600

    actualizacion cobertura

--- src/components/Cobertura/VistaDetalleEstado.jsx
+++ src/components/Cobertura/VistaDetalleEstado.jsx
@@ -157,7 +157,7 @@ const VistaDetalleEstado = props => {
             </Typography>
 
             <Typography align='center' color='#000' paragraph>
-              Información al 31 de marzo de 2023, reportada por las Secretarías Ejecutivas del Sistema Anticorrupción Estatal
+              Información al 30 de septiembre de 2024, reportada por la Secretaría Ejecutiva del Sistema Estatal Anticorrupción 
             </Typography>
 
             <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'stretch' }} justifyContent='center'>
