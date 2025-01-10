7ef1783e261487812c6814ac0c3b7719df5da463
RBalan21 <aln.rojas.pdn@gmail.com>
2025-01-10 12:17:57 -0600

    update fecha 4to trimestre

--- src/components/Cobertura/VistaDetalleEstado.jsx
+++ src/components/Cobertura/VistaDetalleEstado.jsx
@@ -157,7 +157,7 @@ const VistaDetalleEstado = props => {
             </Typography>
 
             <Typography align='center' color='#000' paragraph>
-              Información al 30 de septiembre de 2024, reportada por la Secretaría Ejecutiva del Sistema Estatal Anticorrupción 
+              Información al 31 de diciembre de 2024, reportada por la Secretaría Ejecutiva del Sistema Estatal Anticorrupción 
             </Typography>
 
             <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'stretch' }} justifyContent='center'>
