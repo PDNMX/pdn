042bdf062ccf37f31099182f0af73ee91ad306b1
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2025-07-11 15:23:55 -0600

    update script

--- src/__test__/utils/readFiles/index.js
+++ src/__test__/utils/readFiles/index.js
@@ -9,16 +9,11 @@ const readFiles = () => {
   try {
     const data = fs.readdirSync(_dir, { withFileTypes: true });
     data.map(file => {
-      let data = [];
       if (file.name.includes('.json')) {
         const _file = `${_dir}/${file.name}`;
         const content = jsonfile.readFileSync(_file, { encoding: 'utf8', flag: 'r' });
 
-        content.forEach(d => {
-          data.push(d);
-        });
-
-        info.push({ name: file.name, data });
+        info.push({ name: file.name, data: content });
       }
       return null;
     });
