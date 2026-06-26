cc724cb6977f520d4439d6e4cc8ae70d02df486f
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2026-06-26 14:18:06 -0600

    divide en grupos de 100 los archivos dentro de la carpeta files

---
+++ src/__test__/utils/readFiles/data/organizar.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+cd files
+
+counter=1
+group=1
+
+for file in $(ls -v *.json); do
+  dir="grupo_${group}"
+  mkdir -p "$dir"
+  mv "$file" "$dir/"
+  if [ $((counter % 100)) -eq 0 ]; then
+    ((group++))
+  fi
+  ((counter++))
+done
+
+echo "Organizados $((counter - 1)) archivos en $group grupos."
