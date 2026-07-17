9a9a40d3fd5d64c1828534f0e25c5a7feb05c56d
MauroTalamantes <maurovillagrana33@gmail.com>
2026-07-17 16:49:41 -0600

    chore: upgrade framer motion to 6

--- src/components/HomeV2/ScrollAnimation.jsx
+++ src/components/HomeV2/ScrollAnimation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 
 const ScrollAnimation = ({ children }) => {
